53f0b6081c227c6345d3c4e4ce6fc501f3c9e481
Joscha Henningsen <joscha.henningsen@tum.de>
2021-03-27 10:43:58 +0100

    allow for stat access

--- api/chat.go
+++ api/chat.go
@@ -6,13 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 	"log"
+	"net/http"
 	"strconv"
+	"sync"
 )
 
 var m *melody.Melody
+var stats = map[string]int{}
+var statsLock = sync.RWMutex{}
 
 func configGinChatRouter(router gin.IRoutes) {
 	router.GET("/:vidId/ws", ChatStream)
+	router.GET("/:vidId/stats", ChatStats)
 	if m == nil {
 		log.Printf("creating melody")
 		m = melody.New()
@@ -44,8 +49,33 @@ func configGinChatRouter(router gin.IRoutes) {
 	})
 }
 
+func ChatStats(context *gin.Context) {
+	u, uErr := tools.GetUser(context)
+	if uErr != nil || u.Role != 1 {
+		context.AbortWithStatusJSON(http.StatusForbidden, gin.H{"msg": "stats can currently only be retrieved by admins"})
+		return
+	}
+	vidId := context.Param("vidId")
+	watchers, found := stats[vidId]
+	if !found {
+		context.AbortWithStatusJSON(http.StatusNotFound, gin.H{"msg": "stream not found"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"watchers": watchers})
+}
+
 func ChatStream(c *gin.Context) {
 	ctxMap := make(map[string]interface{}, 1)
+	println(c.Param("vidId") + "joined")
+	statsLock.Lock()
+	stats[c.Param("vidId")] = stats[c.Param("vidId")] + 1
+	statsLock.Unlock()
+	m.HandleClose(func(session *melody.Session, i int, s string) error {
+		statsLock.Lock()
+		stats[c.Param("vidId")] = stats[c.Param("vidId")] - 1
+		statsLock.Unlock()
+		return nil
+	})
 	ctxMap["ctx"] = c
 	_ = m.HandleRequestWithKeys(c.Writer, c.Request, ctxMap)
 }
