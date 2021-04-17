e681de49e9e3b5bf163efa43f274afff55c7d94e
Joscha Henningsen <joscha.henningsen@tum.de>
2021-04-17 17:29:51 +0200

    show units, render respective stream

--- model/stream-unit.go
+++ model/stream-unit.go
@@ -1,6 +1,11 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+	"gorm.io/gorm"
+)
 
 type StreamUnit struct {
 	gorm.Model
@@ -9,4 +14,29 @@ type StreamUnit struct {
 	UnitStart       uint
 	UnitEnd         uint
 	StreamID        uint
+}
+
+func (s StreamUnit) GetUnitDurationMS() uint {
+	return s.UnitEnd - s.UnitStart
+}
+
+func (s StreamUnit) GetRoundedUnitLen() string {
+	lenS := (s.UnitEnd - s.UnitStart) / 1000
+	lenM := lenS / 60
+	lenH := lenM / 60
+	lenM = lenM % 60
+	lenS = lenS % 60
+	if lenH > 0 {
+		return fmt.Sprintf("%2dh, %2dmin", lenH, lenM)
+	}
+	return fmt.Sprintf("%2dmin, %2dsec", lenM, lenS)
+}
+
+func (s StreamUnit) GetDescriptionHTML() string {
+	unsafe := blackfriday.Run([]byte(s.UnitDescription))
+	html := bluemonday.
+		UGCPolicy().
+		AddTargetBlankToFullyQualifiedLinks(true).
+		SanitizeBytes(unsafe)
+	return string(html)
 }
