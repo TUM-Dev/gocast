512081c3fcfd451bb15c5d95e1d4d9e15d3ef564
Joscha Henningsen <joscha.henningsen@tum.de>
2021-10-22 13:10:55 +0200

    trim chat spaces

--- api/chat.go
+++ api/chat.go
@@ -14,6 +14,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -48,6 +49,7 @@ func configGinChatRouter(router *gin.RouterGroup) {
 		if err := json.Unmarshal(msg, &chat); err != nil {
 			return
 		}
+		chat.Msg = strings.TrimSpace(chat.Msg)
 		if chat.Msg == "" || len(chat.Msg) > 200 {
 			return
 		}
