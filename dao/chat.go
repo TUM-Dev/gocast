1bf3e43e41d40ab83cf84da2f2dec586d190b057
Alexander Stephan <alexander.stephan@tum.de>
2021-11-23 22:08:25 +0100

    Revert dao changes to move them in a different PR
    Signed-off-by: Alexander Stephan <alexander.stephan@tum.de>

--- dao/chat.go
+++ dao/chat.go
@@ -9,8 +9,8 @@ func AddMessage(chat model.Chat) {
 }
 
 //IsUserCooledDown returns true if a user sent 5 messages within the last two minutes
-func IsUserCooledDown(uid string) (bool, error) {
-	var count int64
-	err := DB.Table("chats").Where("user_id = ? AND created_at > ADDTIME(NOW(), '-0:02:0')", uid).Count(&count).Error
-	return count >= 5, err
+func IsUserCooledDown(uid string) bool {
+	var chat []model.Chat
+	res := DB.Table("chats").Where("user_id = ? AND created_at > ADDTIME(NOW(), '-0:02:0')", uid).Scan(&chat)
+	return res.RowsAffected >= 5
 }
