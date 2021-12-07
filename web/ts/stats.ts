85fe9de08cc40a2db0107a95836032452e74b4b2
Alexander Stephan <alexander.stephan@tum.de>
2021-12-07 20:05:25 +0100

    Fix dependencies

--- web/ts/stats.ts
+++ web/ts/stats.ts
@@ -1,39 +1,41 @@
 import { StatusCodes } from "http-status-codes";
-import { Chart, registerables} from 'chart.js'
+import { Chart, registerables } from "chart.js";
 Chart.register(...registerables); // TODO: Tree shaking
 
 export function loadStats(endpoint: string, targetEl: string) {
-    const canvas = <HTMLCanvasElement> document.getElementById(targetEl);
-    const ctx = canvas.getContext('2d');
-    getAsync(`/api/course/${(document.getElementById("courseID") as HTMLInputElement).value}/stats?interval=${endpoint}`).then(res => {
-            if (res.status === StatusCodes.OK) {
-                res.text().then(value => {
-                    new Chart(ctx, JSON.parse(value));
-                });
-            }
+    const canvas = <HTMLCanvasElement>document.getElementById(targetEl);
+    const ctx = canvas.getContext("2d");
+    getAsync(
+        `/api/course/${(document.getElementById("courseID") as HTMLInputElement).value}/stats?interval=${endpoint}`,
+    ).then((res) => {
+        if (res.status === StatusCodes.OK) {
+            res.text().then((value) => {
+                new Chart(ctx, JSON.parse(value));
+            });
         }
-    );
+    });
 }
 
 export function initStatsPage() {
     const dates = ["numStudents", "vodViews", "liveViews"];
-    dates.forEach(endpoint => {
-        getAsync(`/api/course/${(document.getElementById("courseID") as HTMLInputElement).value}/stats?interval=${endpoint}`).then(res => {
-                if (res.status === StatusCodes.OK) {
-                    res.text().then(value => {
-                        document.getElementById(endpoint).innerHTML = `<span>${JSON.parse(value)["res"]}</span>`
-                    });
-                }
+    dates.forEach((endpoint) => {
+        getAsync(
+            `/api/course/${(document.getElementById("courseID") as HTMLInputElement).value}/stats?interval=${endpoint}`,
+        ).then((res) => {
+            if (res.status === StatusCodes.OK) {
+                res.text().then((value) => {
+                    document.getElementById(endpoint).innerHTML = `<span>${JSON.parse(value)["res"]}</span>`;
+                });
             }
-        );
+        });
     });
 }
 
-export async function getAsync(url = '') {
+export async function getAsync(url = "") {
     return await fetch(url, {
-        method: 'GET',
+        method: "GET",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
     });
 }
