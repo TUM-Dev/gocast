17914f958d902de91c1e2609aa3bdb7ca0753325
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2021-12-09 09:23:58 +0100

    Update cache comment
    Co-authored-by: Alexander Stephan <alexander.stephan@tum.de>

--- tools/cache.go
+++ tools/cache.go
@@ -10,7 +10,7 @@ var cache *ristretto.Cache
 func initCache() {
 	c, err := ristretto.NewCache(&ristretto.Config{
 		NumCounters: 1e6,     // number of keys to track frequency of (1M).
-		MaxCost:     1 << 29, // 1 << 30 == 1/2GB Cost of cache
+		MaxCost:     1 << 29, // 1 << 29 == 1/2GB Cost of cache
 		BufferItems: 64,
 	})
 	if err != nil {
