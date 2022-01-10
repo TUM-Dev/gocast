1d381f6a33fd2542726e75beb9d4b1f15786a5ef
Alexander Stephan <alexander.stephan@tum.de>
2022-01-10 18:40:36 +0100

    Attempt to make edit-course work

--- web/ts/server-notifications.ts
+++ web/ts/server-notifications.ts
@@ -1,9 +1,10 @@
-class ServerNotifications {
+import flatpickr from "flatpickr";
+
+export class ServerNotifications {
     constructor() {
         ["from", "expires"].forEach((value) => {
             Array.prototype.forEach.call(document.getElementsByClassName(value), function (el) {
                 console.log(el.id);
-                // @ts-ignore
                 flatpickr(`#${el.id}`, { enableTime: true, time_24hr: true });
             });
         });
