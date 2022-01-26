9b4f21ec11483c36a52cbc3f20f99e93df0be1cb
Joscha Henningsen <joscha.henningsen@tum.de>
2022-01-26 13:21:22 +0100

    buffer stream progresses

--- api/progress.go
+++ api/progress.go
@@ -2,13 +2,64 @@ package api
 
 import (
 	"TUM-Live/dao"
+	"TUM-Live/model"
 	"TUM-Live/tools"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"sync"
+	"time"
 )
 
+var progressBuff *progressBuffer
+
+// progressWorker contains progresses to be written to the database
+type progressBuffer struct {
+	Lock       sync.Mutex
+	Progresses []model.StreamProgress
+	interval   time.Duration
+}
+
+func newProgressBuffer() *progressBuffer {
+	return &progressBuffer{
+		Lock:       sync.Mutex{},
+		Progresses: []model.StreamProgress{},
+		interval:   time.Second * 5,
+	}
+}
+
+// add new progress to the list to be flushed eventually
+func (b *progressBuffer) add(progress model.StreamProgress) {
+	b.Lock.Lock()
+	defer b.Lock.Unlock()
+	b.Progresses = append(b.Progresses, progress)
+}
+
+// flush writes the collected progresses to the database
+func (b *progressBuffer) flush() error {
+	b.Lock.Lock()
+	defer b.Lock.Unlock()
+	if len(b.Progresses) == 0 {
+		return nil
+	}
+	err := dao.SaveProgresses(b.Progresses)
+	b.Progresses = []model.StreamProgress{}
+	return err
+}
+
+func (b *progressBuffer) Run() {
+	for {
+		time.Sleep(b.interval)
+		err := b.flush()
+		if err != nil {
+			log.WithError(err).Error("Error flushing progress buffer")
+		}
+	}
+}
+
 func configProgressRouter(router *gin.Engine) {
+	progressBuff = newProgressBuffer()
+	go progressBuff.Run()
 	router.POST("/api/progressReport", saveProgress)
 }
 
@@ -43,11 +94,9 @@ func saveProgress(c *gin.Context) {
 		return
 	}
 
-	err = dao.SaveProgress(request.Progress, tumLiveContext.User.ID, request.StreamID)
-
-	if err != nil {
-		log.WithError(err).Warn("Could not save progress in the database.")
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
+	progressBuff.add(model.StreamProgress{
+		Progress: request.Progress,
+		StreamID: request.StreamID,
+		UserID:   tumLiveContext.User.ID,
+	})
 }
--- dao/progress.go
+++ dao/progress.go
@@ -5,18 +5,12 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-// SaveProgress saves the users progress for a given streamID in the database.
-func SaveProgress(progress float64, userID uint, streamID uint) (err error) {
-	// Update all columns, except primary keys, to new progress on conflict
-	err = DB.Clauses(clause.OnConflict{
+// SaveProgresses saves a slice of stream progresses. If a progress already exists, it will be updated.
+func SaveProgresses(progresses []model.StreamProgress) error {
+	return DB.Debug().Clauses(clause.OnConflict{
 		Columns:   []clause.Column{{Name: "stream_id"}, {Name: "user_id"}}, // key column
 		DoUpdates: clause.AssignmentColumns([]string{"progress"}),          // column needed to be updated
-	}).Create(&model.StreamProgress{
-		Progress: progress,
-		StreamID: streamID,
-		UserID:   userID,
-	}).Error
-	return err
+	}).Create(progresses).Error
 }
 
 // LoadProgress retrieves the current StreamProgress from the database for a given user and stream.
