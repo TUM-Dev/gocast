01fc56a4481f8c5d8d85977cbcabd7e876638172
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2022-03-21 12:06:18 +0100

    use uniqueIndex for unique constraints (#328)

--- model/user.go
+++ model/user.go
@@ -25,8 +25,8 @@ type User struct {
 	gorm.Model
 
 	Name                string         `gorm:"not null"`
-	Email               sql.NullString `gorm:"type:varchar(256); unique; default:null"`
-	MatriculationNumber string         `gorm:"type:varchar(256); unique; default:null"`
+	Email               sql.NullString `gorm:"type:varchar(256); uniqueIndex; default:null"`
+	MatriculationNumber string         `gorm:"type:varchar(256); uniqueIndex; default:null"`
 	LrzID               string
 	Role                uint     `gorm:"default:4"` // AdminType = 1, LecturerType = 2, GenericType = 3, StudentType  = 4
 	Password            string   `gorm:"default:null"`
