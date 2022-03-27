bc77d1d4e0e4ffe0fd06ff816bda6c6ee63a4e4a
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2022-03-27 23:47:32 +0200

    remove old/unused stuff (#332)

--- api/router.go
+++ api/router.go
@@ -2,27 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	_ "github.com/satori/go.uuid"
-	"time"
 )
-
-var (
-	_ = time.Second // import time.Second for unknown usage in api
-)
-
-// PagedResults results for pages GetAll results.
-type PagedResults struct {
-	Page         int64       `json:"Page"`
-	PageSize     int64       `json:"PageSize"`
-	Data         interface{} `json:"Data"`
-	TotalRecords int         `json:"TotalRecords"`
-}
-
-// HTTPError example
-type HTTPError struct {
-	Code    int    `json:"Code" example:"400"`
-	Message string `json:"Message" example:"status bad request"`
-}
 
 // ConfigChatRouter configure gin router for chat (without gzip)
 func ConfigChatRouter(router *gin.RouterGroup) {
