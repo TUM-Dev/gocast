44435be4a3ba5a277f24b2f1a046a5d23a5927e0
Joscha Henningsen <joscha.henningsen@tum.de>
2022-04-25 20:13:31 +0200

    fallback to mwn id if no matr nr is present

--- web/router.go
+++ web/router.go
@@ -6,7 +6,6 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"embed"
-	"fmt"
 	"github.com/crewjam/saml"
 	"github.com/crewjam/saml/samlsp"
 	"github.com/gin-gonic/gin"
@@ -140,6 +139,20 @@ func configSaml(r *gin.Engine) {
 		if lastName != "" {
 			lastNameUser = &lastName
 		}
+		if matrNr == "" {
+			matrNr = extractSamlField(response, "eduPersonPrincipalName") // MWN id if no matrNr
+			s := strings.Split(matrNr, "@")
+			if len(s) == 0 || s[0] == "" {
+				log.WithFields(log.Fields{
+					"LRZ-ID":    lrzID,
+					"firstName": firstName,
+					"lastName":  lastName,
+					"mwnID":     matrNr,
+				}).Error("Can't extract mwn id")
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+			matrNr = s[0]
+		}
 		user := model.User{
 			Name:                firstName,
 			LastName:            lastNameUser,
@@ -154,27 +167,6 @@ func configSaml(r *gin.Engine) {
 		startSession(c, &sessionData{userid: user.ID, samlSubjectID: &subjectID})
 		c.Redirect(http.StatusFound, "/")
 	})
-}
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string // Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)                             // Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host)) // Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	} // Return the request as a string
-	return strings.Join(request, "\n")
 }
 
 func extractSamlField(assertion *saml.Assertion, friendlyFieldName string) string {
