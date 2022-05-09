7a4786247d4e0465bd24c79247ccb39e78b36bdc
Ata Keskin <ata.keskin@tum.de>
2022-05-09 13:57:45 +0200

    Finalized the template and added code for executing the template.

--- api/download_ics.go
+++ api/download_ics.go
@@ -7,15 +7,22 @@ import (
 	"github.com/joschahenningsen/TUM-Live/dao"
 	"github.com/joschahenningsen/TUM-Live/model"
 	"github.com/joschahenningsen/TUM-Live/tools"
+	"html/template"
 	"net/http"
 	"strconv"
 )
 
 func configGinDownloadICSRouter(router *gin.Engine) {
-	router.GET("/api/download_ics/:slug/:term/:year", downloadICS)
+	router.GET("/api/download_ics/:year/:term/:slug", downloadICS)
 }
 
 func downloadICS(c *gin.Context) {
+	templates, err := template.ParseFS(staticFS, "template/*.gotemplate")
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
 	foundContext, exists := c.Get("TUMLiveContext")
 	if !exists {
 		sentry.CaptureException(errors.New("context should exist but doesn't"))
@@ -41,11 +48,57 @@ func downloadICS(c *gin.Context) {
 		return
 	}
 
-	c.Header("content-type", "text/calendar")
+	if len(course.Streams) == 0 {
+		_, err := c.Writer.WriteString("No lectures found!")
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		c.Writer.Flush()
+	}
 
-	c.JSON(http.StatusOK, streamsToICS(course.Streams))
+	var acc []CalendarEntry
+	for _, s := range course.Streams {
+		acc = append(acc, streamToCalendarEntry(s, course))
+	}
+
+	c.Header("content-type", "text/calendar")
+	c.Header("Content-Transfer-Encoding", "binary")
+	c.Header("Content-Disposition", "attachment; filename="+course.Slug+course.TeachingTerm+strconv.Itoa(course.Year)+".ics")
+	err = templates.ExecuteTemplate(c.Writer, "ics.gotemplate", acc)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
 }
 
-func streamsToICS(streams []model.Stream) []model.Stream {
-	return streams
+type CalendarEntry struct {
+	CreatedAt   string
+	Start       string
+	End         string
+	ID          string
+	Url         string
+	Location    string
+	Summary     string
+	Description string
+}
+
+func streamToCalendarEntry(s model.Stream, c model.Course) CalendarEntry {
+	layout := "20060102T150405"
+	var location = ""
+	if len(s.RoomName) > 0 {
+		location += s.RoomName + " "
+	}
+	if len(s.RoomCode) > 0 {
+		location += s.RoomCode
+	}
+	return CalendarEntry{
+		CreatedAt:   s.CreatedAt.Format(layout),
+		Start:       s.Start.Format(layout),
+		End:         s.End.Format(layout),
+		ID:          s.StreamKey,
+		Url:         "https://live.rbg.tum.de/course/" + strconv.Itoa(c.Year) + "/" + c.TeachingTerm + "/" + c.Slug,
+		Location:    location,
+		Summary:     c.Name,
+		Description: s.Name,
+	}
 }
