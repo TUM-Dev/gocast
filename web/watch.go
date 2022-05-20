4ec74659e9823ed90bf80ee9b3a1232b5cfc7031
Ata Keskin <ata.keskin@tum.de>
2022-05-20 18:07:38 +0200

    Reverting the change
     and using the CanUseSource function only to decide whether to display the buttons on vod_course_list.gohtml

--- web/watch.go
+++ web/watch.go
@@ -57,22 +57,22 @@ func (r mainRoutes) WatchPage(c *gin.Context) {
 			}
 		}
 	}
-	/*
-		if tumLiveContext.Stream.LectureHallID != 0 {
-			switch data.IndexData.TUMLiveContext.Course.GetSourceModeForLectureHall(data.IndexData.TUMLiveContext.Stream.LectureHallID) {
-			// I'm not sure if this is the best solution, but it works.
-			// SourceMode == 1 -> Override Version to PRES
-			case 1:
-				data.Version = "PRES"
-				data.IndexData.TUMLiveContext.Stream.PlaylistUrlCAM = ""
-				data.IndexData.TUMLiveContext.Stream.PlaylistUrl = ""
-			// SourceMode == 2 -> Override Version to CAM
-			case 2:
-				data.Version = "CAM"
-				data.IndexData.TUMLiveContext.Stream.PlaylistUrlPRES = ""
-				data.IndexData.TUMLiveContext.Stream.PlaylistUrl = ""
-			}
-		}*/
+
+	if tumLiveContext.Stream.LectureHallID != 0 {
+		switch data.IndexData.TUMLiveContext.Course.GetSourceModeForLectureHall(data.IndexData.TUMLiveContext.Stream.LectureHallID) {
+		// I'm not sure if this is the best solution, but it works.
+		// SourceMode == 1 -> Override Version to PRES
+		case 1:
+			data.Version = "PRES"
+			data.IndexData.TUMLiveContext.Stream.PlaylistUrlCAM = ""
+			data.IndexData.TUMLiveContext.Stream.PlaylistUrl = ""
+		// SourceMode == 2 -> Override Version to CAM
+		case 2:
+			data.Version = "CAM"
+			data.IndexData.TUMLiveContext.Stream.PlaylistUrlPRES = ""
+			data.IndexData.TUMLiveContext.Stream.PlaylistUrl = ""
+		}
+	}
 
 	// Check for fetching progress
 	if tumLiveContext.User != nil && tumLiveContext.Stream.Recording {
