416956d6e07c0c04aa85059c2d7279961cdfef67
Matthias Reumann <matthias.reumann@tum.de>
2022-07-18 06:55:15 +0200

    Update worker tests

--- api/worker_test.go
+++ api/worker_test.go
@@ -2,70 +2,58 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/joschahenningsen/TUM-Live/dao"
 	"github.com/joschahenningsen/TUM-Live/mock_dao"
-	"github.com/joschahenningsen/TUM-Live/model"
-	"github.com/joschahenningsen/TUM-Live/tools"
-	"github.com/stretchr/testify/assert"
+	"github.com/joschahenningsen/TUM-Live/tools/testutils"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestWorker(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
-	t.Run("DELETE[DeleteWorker returns error]", func(t *testing.T) {
-		workerId := "1234"
+	t.Run("DELETE/api/workers/:workerID", func(t *testing.T) {
+		url := fmt.Sprintf("/api/workers/%s", testutils.Worker1.WorkerID)
+		testCases := testutils.TestCases{
+			"can not delete worker": {
+				Method: http.MethodDelete,
+				Url:    url,
+				DaoWrapper: dao.DaoWrapper{
+					WorkerDao: func() dao.WorkerDao {
+						workerDaoMock := mock_dao.NewMockWorkerDao(gomock.NewController(t))
+						workerDaoMock.
+							EXPECT().
+							DeleteWorker(testutils.Worker1.WorkerID).
+							Return(errors.New("")).
+							AnyTimes()
+						return workerDaoMock
+					}(),
+				},
+				TumLiveContext: &testutils.TUMLiveContextAdmin,
+				ExpectedCode:   http.StatusInternalServerError,
+			},
+			"success": {
+				Method: http.MethodDelete,
+				Url:    url,
+				DaoWrapper: dao.DaoWrapper{
+					WorkerDao: func() dao.WorkerDao {
+						workerDaoMock := mock_dao.NewMockWorkerDao(gomock.NewController(t))
+						workerDaoMock.
+							EXPECT().
+							DeleteWorker(testutils.Worker1.WorkerID).
+							Return(nil).
+							AnyTimes()
+						return workerDaoMock
+					}(),
+				},
+				TumLiveContext: &testutils.TUMLiveContextAdmin,
+				ExpectedCode:   http.StatusOK,
+			},
+		}
 
-		w := httptest.NewRecorder()
-		c, r := gin.CreateTestContext(w)
-
-		// Middleware to set Mock-TUMLiveContext
-		r.Use(func(c *gin.Context) {
-			c.Set("TUMLiveContext", tools.TUMLiveContext{User: &model.User{
-				Name: "Admin",
-				Role: model.AdminType,
-			}})
-		})
-
-		ctrl := gomock.NewController(t)
-		workerDaoMock := mock_dao.NewMockWorkerDao(ctrl)
-		workerDaoMock.EXPECT().DeleteWorker(workerId).Return(errors.New("")).AnyTimes()
-
-		configWorkerRouter(r, dao.DaoWrapper{WorkerDao: workerDaoMock})
-
-		c.Request, _ = http.NewRequest(http.MethodDelete, "/api/workers/"+workerId, nil)
-		r.ServeHTTP(w, c.Request)
-
-		assert.Equal(t, http.StatusInternalServerError, w.Code)
-	})
-
-	t.Run("DELETE[success]", func(t *testing.T) {
-		workerId := "1234"
-
-		w := httptest.NewRecorder()
-		c, r := gin.CreateTestContext(w)
-
-		// Middleware to set Mock-TUMLiveContext
-		r.Use(func(c *gin.Context) {
-			c.Set("TUMLiveContext", tools.TUMLiveContext{User: &model.User{
-				Name: "Admin",
-				Role: model.AdminType,
-			}})
-		})
-
-		ctrl := gomock.NewController(t)
-		workerDaoMock := mock_dao.NewMockWorkerDao(ctrl)
-		workerDaoMock.EXPECT().DeleteWorker(workerId).Return(nil).AnyTimes()
-
-		configWorkerRouter(r, dao.DaoWrapper{WorkerDao: workerDaoMock})
-
-		c.Request, _ = http.NewRequest(http.MethodDelete, "/api/workers/"+workerId, nil)
-		r.ServeHTTP(w, c.Request)
-
-		assert.Equal(t, http.StatusOK, w.Code)
+		testCases.Run(t, configWorkerRouter)
 	})
 }
