0230094e424468c16681303ea7cfcd30c701428d
Matthias Reumann <matthias.reumann@tum.de>
2022-07-25 10:01:47 +0200

    Update token tests

--- api/token_test.go
+++ api/token_test.go
@@ -1,124 +1,127 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/joschahenningsen/TUM-Live/dao"
 	"github.com/joschahenningsen/TUM-Live/mock_dao"
 	"github.com/joschahenningsen/TUM-Live/model"
-	"github.com/joschahenningsen/TUM-Live/tools"
 	"github.com/joschahenningsen/TUM-Live/tools/testutils"
+	"github.com/matthiasreumann/gomino"
 	"net/http"
 	"testing"
 	"time"
 )
 
+func TokenRouterWrapper(r *gin.Engine) {
+	configTokenRouter(r, dao.DaoWrapper{})
+}
+
 func TestToken(t *testing.T) {
 	t.Run("/create", func(t *testing.T) {
+		url := "/api/token/create"
+
 		now := time.Now()
 		type req struct {
 			Expires *time.Time `json:"expires"`
 			Scope   string     `json:"scope"`
 		}
-		testCases := testutils.TestCases{
-			"POST[No Context]": testutils.TestCase{
-				Method:         http.MethodPost,
-				Url:            "/api/token/create",
-				DaoWrapper:     dao.DaoWrapper{},
-				TumLiveContext: nil,
-				ExpectedCode:   http.StatusInternalServerError,
-			},
-
-			"POST[Invalid Body]": testutils.TestCase{
-				Method:         http.MethodPost,
-				Url:            "/api/token/create",
-				DaoWrapper:     dao.DaoWrapper{},
-				TumLiveContext: &tools.TUMLiveContext{User: &model.User{Role: model.AdminType}},
-				Body:           bytes.NewBuffer([]byte{}),
-				ExpectedCode:   http.StatusBadRequest,
-			},
-			"POST[Invalid Scope]": testutils.TestCase{
-				Method:         http.MethodPost,
-				Url:            "/api/token/create",
-				DaoWrapper:     dao.DaoWrapper{},
-				TumLiveContext: &tools.TUMLiveContext{User: &model.User{Role: model.AdminType}},
-				Body: bytes.NewBuffer(testutils.First(json.Marshal(req{
-					Expires: &now,
-					Scope:   "invalid",
-				})).([]byte)),
-				ExpectedCode: http.StatusBadRequest,
-			},
-			"POST[AddToken returns error]": testutils.TestCase{
-				Method: http.MethodPost,
-				Url:    "/api/token/create",
-				DaoWrapper: dao.DaoWrapper{
-					TokenDao: func() dao.TokenDao {
-						tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
-						tokenMock.EXPECT().AddToken(gomock.Any()).Return(errors.New("")).AnyTimes()
-						return tokenMock
-					}(),
-				},
-				TumLiveContext: &tools.TUMLiveContext{User: &model.User{Role: model.AdminType}},
-				Body: bytes.NewBuffer(testutils.First(json.Marshal(req{
-					Expires: &now,
-					Scope:   model.TokenScopeAdmin,
-				})).([]byte)),
+		gomino.TestCases{
+			"POST[No Context]": {
+				Router:       TokenRouterWrapper,
+				Method:       http.MethodPost,
+				Url:          url,
 				ExpectedCode: http.StatusInternalServerError,
 			},
-			"POST[success]": testutils.TestCase{
-				Method: http.MethodPost,
-				Url:    "/api/token/create",
-				DaoWrapper: dao.DaoWrapper{
-					TokenDao: func() dao.TokenDao {
-						tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
-						tokenMock.EXPECT().AddToken(gomock.Any()).Return(nil).AnyTimes()
-						return tokenMock
-					}(),
+
+			"POST[Invalid Body]": {
+				Router:       TokenRouterWrapper,
+				Method:       http.MethodPost,
+				Url:          url,
+				Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+				ExpectedCode: http.StatusBadRequest,
+			},
+			"POST[Invalid Scope]": {
+				Router:       TokenRouterWrapper,
+				Method:       http.MethodPost,
+				Url:          url,
+				Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+				Body:         req{Expires: &now, Scope: "invalid"},
+				ExpectedCode: http.StatusBadRequest,
+			},
+			"POST[AddToken returns error]": {
+				Router: func(r *gin.Engine) {
+					wrapper := dao.DaoWrapper{
+						TokenDao: func() dao.TokenDao {
+							tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
+							tokenMock.EXPECT().AddToken(gomock.Any()).Return(errors.New("")).AnyTimes()
+							return tokenMock
+						}(),
+					}
+					configTokenRouter(r, wrapper)
 				},
-				TumLiveContext: &tools.TUMLiveContext{User: &model.User{Role: model.AdminType}},
-				Body: bytes.NewBuffer(testutils.First(json.Marshal(req{
-					Expires: &now,
-					Scope:   model.TokenScopeAdmin,
-				})).([]byte)),
+				Method:       http.MethodPost,
+				Url:          url,
+				Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+				Body:         req{Expires: &now, Scope: model.TokenScopeAdmin},
+				ExpectedCode: http.StatusInternalServerError,
+			},
+			"POST[success]": {
+				Router: func(r *gin.Engine) {
+					wrapper := dao.DaoWrapper{
+						TokenDao: func() dao.TokenDao {
+							tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
+							tokenMock.EXPECT().AddToken(gomock.Any()).Return(nil).AnyTimes()
+							return tokenMock
+						}(),
+					}
+					configTokenRouter(r, wrapper)
+				},
+				Method:       http.MethodPost,
+				Url:          url,
+				Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+				Body:         req{Expires: &now, Scope: model.TokenScopeAdmin},
 				ExpectedCode: http.StatusOK,
 			},
-		}
-
-		testCases.Run(t, configTokenRouter)
+		}.Run(t, testutils.Equal)
 	})
 
 	t.Run("/:id", func(t *testing.T) {
-		testCases := testutils.TestCases{
-			"DELETE[DeleteToken returns error]": testutils.TestCase{
-				Method: http.MethodDelete,
-				Url:    "/api/token/1",
-				DaoWrapper: dao.DaoWrapper{
-					TokenDao: func() dao.TokenDao {
-						tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
-						tokenMock.EXPECT().DeleteToken("1").Return(errors.New("")).AnyTimes()
-						return tokenMock
-					}(),
+		url := "/api/token/1"
+		gomino.TestCases{
+			"DELETE[DeleteToken returns error]": {
+				Router: func(r *gin.Engine) {
+					wrapper := dao.DaoWrapper{
+						TokenDao: func() dao.TokenDao {
+							tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
+							tokenMock.EXPECT().DeleteToken("1").Return(errors.New("")).AnyTimes()
+							return tokenMock
+						}(),
+					}
+					configTokenRouter(r, wrapper)
 				},
-				TumLiveContext: &tools.TUMLiveContext{User: &model.User{Role: model.AdminType}},
-				ExpectedCode:   http.StatusInternalServerError,
+				Method:       http.MethodDelete,
+				Url:          url,
+				Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+				ExpectedCode: http.StatusInternalServerError,
 			},
-			"DELETE[Success]": testutils.TestCase{
-				Method: http.MethodDelete,
-				Url:    "/api/token/1",
-				DaoWrapper: dao.DaoWrapper{
-					TokenDao: func() dao.TokenDao {
-						tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
-						tokenMock.EXPECT().DeleteToken("1").Return(nil).AnyTimes()
-						return tokenMock
-					}(),
+			"DELETE[Success]": {
+				Router: func(r *gin.Engine) {
+					wrapper := dao.DaoWrapper{
+						TokenDao: func() dao.TokenDao {
+							tokenMock := mock_dao.NewMockTokenDao(gomock.NewController(t))
+							tokenMock.EXPECT().DeleteToken("1").Return(nil).AnyTimes()
+							return tokenMock
+						}(),
+					}
+					configTokenRouter(r, wrapper)
 				},
-				TumLiveContext: &tools.TUMLiveContext{User: &model.User{Role: model.AdminType}},
-				ExpectedCode:   http.StatusOK,
+				Method:       http.MethodDelete,
+				Url:          url,
+				Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+				ExpectedCode: http.StatusOK,
 			},
-		}
-
-		testCases.Run(t, configTokenRouter)
+		}.Run(t, testutils.Equal)
 	})
 }
