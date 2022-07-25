c6fd5d28eeb600f6a8e383acaca4b331b3a3314a
Matthias Reumann <matthias.reumann@tum.de>
2022-07-25 09:23:31 +0200

    Update audit tests

--- api/audit_test.go
+++ api/audit_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/joschahenningsen/TUM-Live/mock_dao"
 	"github.com/joschahenningsen/TUM-Live/model"
 	"github.com/joschahenningsen/TUM-Live/tools/testutils"
+	"github.com/matthiasreumann/gomino"
 	"net/http"
 	"testing"
 )
@@ -19,14 +20,16 @@ func TestGetAudits(t *testing.T) {
 	mock.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return([]model.Audit{}, nil).AnyTimes()
 	mock.EXPECT().Create(gomock.Any()).Return(nil).AnyTimes()
 
-	testCases := testutils.TestCases{
+	gomino.TestCases{
 		"get audits": {
-			Method:         http.MethodGet,
-			Url:            "/api/audits?limit=1&offset=0&types[]=1",
-			DaoWrapper:     dao.DaoWrapper{AuditDao: mock},
-			TumLiveContext: &testutils.TUMLiveContextAdmin,
-			ExpectedCode:   http.StatusOK,
+			Router: func(r *gin.Engine) {
+				wrapper := dao.DaoWrapper{AuditDao: mock}
+				configAuditRouter(r, wrapper)
+			},
+			Method:       http.MethodGet,
+			Url:          "/api/audits?limit=1&offset=0&types[]=1",
+			Middlewares:  testutils.TUMLiveMiddleware(testutils.TUMLiveContextAdmin),
+			ExpectedCode: http.StatusOK,
 		},
-	}
-	testCases.Run(t, configAuditRouter)
+	}.Run(t, testutils.Equal)
 }
