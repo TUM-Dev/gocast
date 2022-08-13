22c272bd100ee8cd4fd023de2d0442229f375632
Maximilian Weyand <m.weyand@tum.de>
2022-08-14 01:36:25 +0200

    update test to new test architecture

--- api/chat_test.go
+++ api/chat_test.go
@@ -19,6 +19,16 @@ func ChatRouterWrapper(r *gin.Engine) {
 	configGinChatRouter(r.Group("/api/chat"), dao.DaoWrapper{})
 }
 
+func ChatDefaultRouter(t *testing.T) func(r *gin.Engine) {
+	return func(r *gin.Engine) {
+		wrapper := dao.DaoWrapper{
+			StreamsDao: testutils.GetStreamMock(t),
+			CoursesDao: testutils.GetCoursesMock(t),
+		}
+		configGinChatRouter(r.Group("/api/chat"), wrapper)
+	}
+}
+
 func TestMessages(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	t.Run("GET/api/chat/:streamID/messages", func(t *testing.T) {
@@ -206,85 +216,44 @@ func TestUsers(t *testing.T) {
 
 func TestChatAccessChecker(t *testing.T) {
 	gin.SetMode(gin.TestMode)
+
 	tests := []struct {
 		name              string
 		courseChatEnabled bool
 		streamChatEnabled bool
-		isAdmin           bool
 	}{
-		{"ChatEnabled=false stream", true, false, true},
-		{"ChatEnabled=false course", false, true, true},
-		{"ChatEnabled=false course stream", false, false, true},
-		{"ChatEnabled=false stream", true, false, false},
-		{"ChatEnabled=false course", false, true, false},
-		{"ChatEnabled=false course stream", false, false, false},
+		{"ChatEnabled=false stream", true, false},
+		{"ChatEnabled=false course", false, true},
+		{"ChatEnabled=false course stream", false, false},
+	}
+
+	transformContext := func(ctx tools.TUMLiveContext, streamChatEnabled bool, courseChatEnabled bool) func(c *gin.Context) {
+		ctx.Stream = &testutils.StreamFPVLive
+		ctx.Stream.ChatEnabled = streamChatEnabled
+		ctx.Course = &testutils.CourseFPV
+		ctx.Course.ChatEnabled = courseChatEnabled
+		return testutils.TUMLiveContext(ctx)
 	}
 
 	apiEndpoints := []string{"/messages", "/active-poll", "/users", "/ws"}
 
-	for _, test := range tests {
-		for _, apiEndpoint := range apiEndpoints {
-			t.Run(fmt.Sprintf("GET[apiEndpoint=%s CourseChatEnabled=%t StreamChatEnabled=%t isAdmin=%t]", apiEndpoint, test.courseChatEnabled, test.streamChatEnabled, test.isAdmin), func(t *testing.T) {
-				uid := uint(1)
-				streamId := uint(1234)
-				courseId := uint(1111)
-				chats := []model.Chat{
-					{Message: "1", IsVisible: false},
-					{Message: "2", IsVisible: true},
-					{Message: "3", IsVisible: true},
-				}
+	for _, apiEndpoint := range apiEndpoints {
+		t.Run(fmt.Sprintf("GET/api/chat/:streamID%s", apiEndpoint), func(t *testing.T) {
+			for _, test := range tests {
+				url := fmt.Sprintf("/api/chat/%d%s", testutils.StreamFPVLive.ID, apiEndpoint)
 
-				w := httptest.NewRecorder()
-				c, r := gin.CreateTestContext(w)
-
-				name := "Hansi"
-				role := model.StudentType
-				if test.isAdmin {
-					name = "Admin"
-					role = model.AdminType
-				}
-
-				r.Use(func(c *gin.Context) {
-					c.Set("TUMLiveContext", tools.TUMLiveContext{User: &model.User{
-						Model: gorm.Model{ID: uid},
-						Name:  name,
-						Role:  uint(role),
-					}})
-				})
-
-				// chat mock
-				chatMock := mock_dao.NewMockChatDao(gomock.NewController(t))
-				chatMock.EXPECT().GetAllChats(uid, streamId).Return(chats, nil).AnyTimes()
-
-				// streams mock
-				streamsMock := mock_dao.NewMockStreamsDao(gomock.NewController(t))
-				streamsMock.EXPECT().GetStreamByID(gomock.Any(), fmt.Sprintf("%d", streamId)).Return(model.Stream{
-					Model:       gorm.Model{ID: streamId},
-					CourseID:    courseId,
-					ChatEnabled: test.streamChatEnabled,
-				}, nil).AnyTimes()
-
-				// course mock
-				courseMock := mock_dao.NewMockCoursesDao(gomock.NewController(t))
-
-				course := model.Course{ChatEnabled: test.courseChatEnabled}
-
-				if !test.isAdmin {
-					course.Visibility = "public"
-				}
-
-				courseMock.EXPECT().GetCourseById(gomock.Any(), courseId).Return(course, nil).AnyTimes()
-
-				chat := r.Group("/api/chat")
-				configGinChatRouter(chat, dao.DaoWrapper{ChatDao: chatMock, StreamsDao: streamsMock, CoursesDao: courseMock})
-
-				endpoint := fmt.Sprintf("/api/chat/%d%s", streamId, apiEndpoint)
-				c.Request, _ = http.NewRequest(http.MethodGet, endpoint, nil)
-				r.ServeHTTP(w, c.Request)
-
-				assert.Equal(t, http.StatusForbidden, w.Code)
-				assert.Equal(t, "", w.Body.String())
-			})
-		}
+				gomino.TestCases{
+					test.name: {
+						Middlewares: testutils.GetMiddlewares(tools.ErrorHandler,
+							transformContext(testutils.TUMLiveContextStudent, test.streamChatEnabled, test.courseChatEnabled)),
+						ExpectedCode: http.StatusForbidden,
+					},
+				}.
+					Method(http.MethodGet).
+					Router(ChatDefaultRouter(t)).
+					Url(url).
+					Run(t, testutils.Equal)
+			}
+		})
 	}
 }
