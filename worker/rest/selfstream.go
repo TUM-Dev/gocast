b217b3d02f617787b356bc5e6ed0e6d682866334
Joscha Henningsen <joscha.henningsen@tum.de>
2022-09-07 10:03:38 +0200

    add debug output

--- worker/rest/selfstream.go
+++ worker/rest/selfstream.go
@@ -1,9 +1,11 @@
 package rest
 
 import (
+	"fmt"
 	"google.golang.org/grpc"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/joschahenningsen/TUM-Live/worker/cfg"
 	"github.com/joschahenningsen/TUM-Live/worker/pb"
@@ -55,9 +57,34 @@ func (s *safeStreams) onPublishDone(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	request = append(request, "remote addr: "+r.RemoteAddr)
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	return strings.Join(request, "\n")
+}
+
 // onPublish is called by nginx when the stream starts publishing
 func (s *safeStreams) onPublish(w http.ResponseWriter, r *http.Request) {
 	log.Info("onPublish called")
+	fmt.Println(formatRequest(r))
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
