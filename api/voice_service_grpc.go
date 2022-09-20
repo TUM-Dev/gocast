69685c26419e4a1c40b6b619038751ed9dd38ca2
Matthias Reumann <matthias.reumann@tum.de>
2022-09-20 10:47:49 +0200

    Add receiver to tum-live

--- api/voice_service_grpc.go
+++ api/voice_service_grpc.go
@@ -1,5 +1,6 @@
-package voice_service
+package api
 
+// voice_service_grpc.go handles communication between tum-live and voice-service
 import (
 	"context"
 	"fmt"
@@ -12,19 +13,21 @@ import (
 	"time"
 )
 
-type server struct {
+type subtitleReceiverServer struct {
 	pb.UnimplementedSubtitleReceiverServer
 }
 
-func (s server) Receive(ctx context.Context, request *pb.ReceiveRequest) (*pb.Empty, error) {
+func (s subtitleReceiverServer) Receive(ctx context.Context, request *pb.ReceiveRequest) (*pb.Empty, error) {
 	fmt.Println(request.GetSubtitles())
 	return &pb.Empty{}, nil
 }
 
-func InitVoiceService(addr string) {
-	lis, err := net.Listen("tcp", addr)
+func init() {
+	log.Info("starting grpc voice-receiver")
+	lis, err := net.Listen("tcp", ":50053")
 	if err != nil {
-		log.WithError(err).Fatal("failed to listen")
+		log.WithError(err).Error("failed to init voice-receiver server")
+		return
 	}
 	grpcServer := grpc.NewServer(grpc.KeepaliveParams(keepalive.ServerParameters{
 		MaxConnectionIdle:     time.Minute,
@@ -33,10 +36,13 @@ func InitVoiceService(addr string) {
 		Time:                  time.Minute * 10,
 		Timeout:               time.Second * 20,
 	}))
-	pb.RegisterSubtitleReceiverServer(grpcServer, &server{})
+	pb.RegisterSubtitleReceiverServer(grpcServer, &subtitleReceiverServer{})
 
 	reflection.Register(grpcServer)
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+		log.Info("dead")
+	}()
 }
