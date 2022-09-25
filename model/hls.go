e706991e8b948565b4424d99143d9d6e7fb65b05
Joscha Henningsen <joscha.henningsen@tum.de>
2022-09-25 23:35:13 +0200

    set column length for streamVersion

--- model/hls.go
+++ model/hls.go
@@ -6,7 +6,7 @@ import "gorm.io/gorm"
 type Hls struct {
 	gorm.Model
 
-	StreamVersion StreamVersion `gorm:"column:stream_version;type:text;not null;default:COMB;primaryKey"`
+	StreamVersion StreamVersion `gorm:"column:stream_version;type:varchar(255);not null;default:COMB;primaryKey"`
 	StreamID      uint          `gorm:"primaryKey;autoIncrement:false"`
 
 	Path string `gorm:"column:path;type:text;not null"` // the path containing playlist.m3u8 and segment0000x.ts
