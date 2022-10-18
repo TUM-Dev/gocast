db70181d6fb8804bd39436be64dc08efd1e73bad
Matthias Reumann <matthias.reumann@tum.de>
2022-10-18 10:31:45 +0200

    Add bookmark form

---
+++ web/ts/bookmarks.ts
@@ -0,0 +1,49 @@
+import { Delete, postData, putData } from "./global";
+
+export const Bookmarks = {
+    add: (request: AddBookmarkRequest) => {
+        postData("/api/bookmarks", request)
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw Error(resp.statusText);
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    },
+
+    update: (bookmarkId: number, request: UpdateBookmarkRequest) => {
+        putData("/api/bookmarks/" + bookmarkId, request)
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw Error(resp.statusText);
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    },
+
+    delete: (bookmarkId: number) => {
+        Delete("/api/bookmarks/" + bookmarkId)
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw Error(resp.statusText);
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    },
+};
+
+class AddBookmarkRequest {
+    StreamID: number;
+    Description: string;
+    Hours: number;
+    Minutes: number;
+    Seconds: number;
+}
+
+class UpdateBookmarkRequest {}
