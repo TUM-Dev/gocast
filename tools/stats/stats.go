b1142e31c00f77c672e48c3298926a1ce1a87593
Khadim <khadimfall0602@gmail.com>
2022-10-29 13:13:09 +0200

    GetCourseNumVodViews & GetCourseNumLiveViews

--- tools/stats/stats.go
+++ tools/stats/stats.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"context"
 	"fmt"
 	influxdb2 "github.com/influxdata/influxdb-client-go"
 	"github.com/influxdata/influxdb-client-go/api"
@@ -44,4 +45,36 @@ func (s *Stats) AddStreamVODStat(courseId string, streamId string) {
 
 	s.liveStats.WritePoint(p)
 	s.liveStats.Flush()
+}
+
+// GetCourseNumVodViews returns the sum of vod views of a course
+func (s *Stats) GetCourseNumVodViews(courseID uint, from time.Time, to time.Time) (int, error) {
+	query := fmt.Sprintf(`from(bucket: "live_stats")
+	|> range(start: %d, stop: %d)
+	|> filter(fn: (r) => r.course == "%d" and r.live == "false")
+	|> keep(columns: ["_value"])
+	|> sum()`, from.Unix(), to.Unix(), courseID)
+
+	if res, err := s.query.Query(context.Background(), query); err != nil {
+		return 0, err
+	} else {
+		return res.Record().Value().(int), nil
+	}
+}
+
+// GetCourseNumLiveViews returns the sum of live views of a course based on the maximum views per lecture
+func (s *Stats) GetCourseNumLiveViews(courseID uint, from time.Time, to time.Time) (int, error) {
+	query := fmt.Sprintf(`from(bucket: "live_stats")
+	|> range(start: %d, stop: %d)
+	|> filter(fn: (r) => r.course == "%d" and r.live == "true") 
+	|> group(columns: ["stream"])
+	|> sum(column: "_value")
+	|> keep(columns: ["_value"])
+	|> max()`, from.Unix(), to.Unix(), courseID)
+
+	if res, err := s.query.Query(context.Background(), query); err != nil {
+		return 0, err
+	} else {
+		return res.Record().Value().(int), nil
+	}
 }
