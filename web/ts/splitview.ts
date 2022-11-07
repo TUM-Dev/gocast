ce5205b7bb7ba40f936b72c79271ed5de028ec69
Matthias Reumann <matthias.reumann@tum.de>
2022-11-07 17:26:33 +0100

    Add dynamic controlbars

--- web/ts/splitview.ts
+++ web/ts/splitview.ts
@@ -1,5 +1,8 @@
+import { getPlayers } from "./TUMLiveVjs";
+
 export class SplitView {
     private camPercentage: number;
+    private players: any[];
 
     showSplitMenu: boolean;
 
@@ -17,17 +20,33 @@ export class SplitView {
     constructor() {
         this.camPercentage = SplitView.Options.SplitEvenly;
         this.showSplitMenu = false;
-        this.updateWidths();
+        this.players = getPlayers();
+        this.updateState();
     }
 
-    updateWidths() {
+    updateState() {
         this.widthCam = this.camPercentage;
         this.widthPres = 100 - this.widthCam;
+
+        let i = 0,
+            j = 1;
+        if (
+            this.camPercentage === SplitView.Options.FullCamera ||
+            this.camPercentage === SplitView.Options.FocusCamera
+        ) {
+            (i = 1), (j = 0);
+        }
+        this.players[j].controlBar.hide();
+        this.players[i].controlBar.show();
     }
 
     update(percentage: number) {
         this.camPercentage = percentage;
-        this.updateWidths();
+        this.updateState();
+    }
+
+    hideMenu() {
+        this.showSplitMenu = false;
     }
 
     toggleMenu() {
