b017b7622dc8174bcff10544c0ede78de6d1378b
Matthias Reumann <matthias.reumann@tum.de>
2022-11-28 08:29:28 +0100

    Update dao function

--- dao/subtitles.go
+++ dao/subtitles.go
@@ -2,9 +2,9 @@ package dao
 
 import (
 	"context"
+	"errors"
 	"github.com/joschahenningsen/TUM-Live/model"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 //go:generate mockgen -source=subtitles.go -destination ../mock_dao/subtitles.go
@@ -16,8 +16,11 @@ type SubtitlesDao interface {
 	// GetByStreamIDandLang returns the subtitles for a given query
 	GetByStreamIDandLang(context.Context, uint, string) (model.Subtitles, error)
 
-	// Create or Update a new Subtitles for the database
+	// CreateOrUpsert creates or updates subtitles for the database
 	CreateOrUpsert(context.Context, *model.Subtitles) error
+
+	// Create saves subtitles
+	Create(c context.Context, it *model.Subtitles) error
 
 	// Delete a Subtitles by id.
 	Delete(context.Context, uint) error
@@ -33,19 +36,41 @@ func NewSubtitlesDao() SubtitlesDao {
 
 // Get a Subtitles by id.
 func (d subtitlesDao) Get(c context.Context, id uint) (res model.Subtitles, err error) {
-	return res, d.db.WithContext(c).First(&res, id).Error
+	return res, DB.WithContext(c).First(&res, id).Error
 }
 
 func (d subtitlesDao) GetByStreamIDandLang(c context.Context, id uint, lang string) (res model.Subtitles, err error) {
-	return res, d.db.WithContext(c).First(&res, &model.Subtitles{StreamID: id, Language: lang}).Error
+	return res, DB.WithContext(c).First(&res, &model.Subtitles{StreamID: id, Language: lang}).Error
 }
 
 // CreateOrUpsert creates or upserts subtitles.
 func (d subtitlesDao) CreateOrUpsert(c context.Context, it *model.Subtitles) error {
-	return d.db.Clauses(clause.OnConflict{UpdateAll: true}).WithContext(c).Create(it).WithContext(c).Error
+	update := DB.
+		WithContext(c).
+		Model(&model.Subtitles{}).Where("stream_id = ? AND language = ?", it.StreamID, it.Language).
+		Update("content", it.Content)
+
+	if update.Error != nil {
+		if errors.Is(update.Error, gorm.ErrRecordNotFound) {
+			return d.Create(c, it)
+		}
+
+		return update.Error
+	}
+
+	if update.RowsAffected == 0 {
+		return d.Create(c, it)
+	}
+
+	return nil
+}
+
+// Create subtitles.
+func (d subtitlesDao) Create(c context.Context, it *model.Subtitles) error {
+	return DB.WithContext(c).Create(it).Error
 }
 
 // Delete a Subtitles by id.
 func (d subtitlesDao) Delete(c context.Context, id uint) error {
-	return d.db.WithContext(c).Delete(&model.Subtitles{}, id).Error
+	return DB.WithContext(c).Delete(&model.Subtitles{}, id).Error
 }
