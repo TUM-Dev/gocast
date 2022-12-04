0543934add8a67fb5da1e5d9bdf6341f64e5f414
Joscha Henningsen <joscha.henningsen@tum.de>
2022-12-04 17:33:51 +0100

    refactor cronService

---
+++ tools/cron.go
@@ -0,0 +1,39 @@
+package tools
+
+import "github.com/robfig/cron/v3"
+
+type CronService struct {
+	cronJobs map[string]func()
+	cron     *cron.Cron
+}
+
+// Cron is the global CronService
+var Cron *CronService
+
+// InitCronService creates an instance of CronService
+func InitCronService() {
+	Cron = &CronService{
+		cronJobs: make(map[string]func(), 0),
+		cron:     cron.New(),
+	}
+}
+
+// AddFunc creates a cronJob fn running at the interval specified by spec. The job can be referenced by name.
+func (c *CronService) AddFunc(name string, fn func(), spec string) error {
+	c.cronJobs[name] = fn
+	_, err := c.cron.AddFunc(spec, fn)
+	return err
+}
+
+// Run starts the CronService
+func (c *CronService) Run() {
+	c.cron.Start()
+}
+
+// RunJob executes the cronJob identified by name even when it's not due.
+// Invalid names are ignored silently.
+func (c *CronService) RunJob(name string) {
+	if job, ok := c.cronJobs[name]; ok {
+		go job()
+	}
+}
