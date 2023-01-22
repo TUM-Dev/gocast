36719c7f382cd614c83f00adc7669528c8629c66
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2023-01-22 09:59:33 +0100

    allow getting stream from edge server with admin token, add authentication tests (#890)
    * allow getting stream from edge server with admin token
    
    * add tests and benchmarks
    
    * Update worker/edge/README.md
    
    Co-authored-by: Alexander Stephan <alexander.stephan@tum.de>
    
    * Update worker/edge/README.md
    
    Co-authored-by: Alexander Stephan <alexander.stephan@tum.de>
    
    * Update worker/edge/edge.go
    
    Co-authored-by: Alexander Stephan <alexander.stephan@tum.de>
    
    * Update worker/edge/edge_test.go
    
    Co-authored-by: Alexander Stephan <alexander.stephan@tum.de>
    
    Co-authored-by: Alexander Stephan <alexander.stephan@tum.de>

---
+++ worker/edge/edge_test.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const testURL = "http://localhost/vod/test.mp4/playlist.m3u8"
+
+func BenchmarkValidateToken(b *testing.B) {
+	str, err := prepareJWT(time.Hour, testURL)
+	if err != nil {
+		b.Fatal(err)
+	}
+	r, _ := http.NewRequest(http.MethodGet, testURL+"?jwt="+str, nil)
+	r.URL.Query().Add("jwt", "abc")
+	w := httptest.NewRecorder()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		validateToken(w, r)
+	}
+}
+
+func TestValidateTokenSuccess(t *testing.T) {
+	str, err := prepareJWT(time.Hour, testURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", testURL+"?jwt="+str, nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); !res {
+		t.Error("validateToken returned false")
+	}
+
+	if w.Code != http.StatusOK {
+		t.Errorf("expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+}
+
+func TestValidateTokenNoJWT(t *testing.T) {
+	_, err := prepareJWT(time.Hour, testURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", testURL, nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); res {
+		t.Error("validateToken returned true")
+	}
+
+	if w.Code != http.StatusForbidden {
+		t.Errorf("expected status code %d, got %d", http.StatusForbidden, w.Code)
+	}
+}
+
+func TestValidateTokenExpiredJWT(t *testing.T) {
+	str, err := prepareJWT(-time.Hour, testURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", testURL+"?jwt="+str, nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); res {
+		t.Error("validateToken returned true")
+	}
+
+	if w.Code != http.StatusForbidden {
+		t.Errorf("expected status code %d, got %d", http.StatusForbidden, w.Code)
+	}
+}
+
+func TestValidateTokenBadURL(t *testing.T) {
+	str, err := prepareJWT(time.Hour, "-- not a url --")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", testURL+"?jwt="+str, nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); res {
+		t.Error("validateToken returned true")
+	}
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("expected status code %d, got %d", http.StatusBadRequest, w.Code)
+	}
+}
+
+func TestValidateTokenIncorrectURL(t *testing.T) {
+	str, err := prepareJWT(time.Hour, testURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", "http://localhost/vod/wrong_video.mp4/playlist.m3u8?jwt="+str, nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); res {
+		t.Error("validateToken returned true")
+	}
+
+	if w.Code != http.StatusForbidden {
+		t.Errorf("expected status code %d, got %d", http.StatusForbidden, w.Code)
+	}
+}
+
+func TestValidateTokenBadJWT(t *testing.T) {
+	_, err := prepareJWT(time.Hour, testURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", "http://localhost/vod/wrong_video.mp4/playlist.m3u8?jwt=abc", nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); res {
+		t.Error("validateToken returned true")
+	}
+
+	if w.Code != http.StatusForbidden {
+		t.Errorf("expected status code %d, got %d", http.StatusForbidden, w.Code)
+	}
+}
+
+func TestValidateTokenAdminToken(t *testing.T) {
+	_, err := prepareJWT(time.Hour, testURL)
+	adminToken = "abcd"
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("GET", "http://localhost/vod/wrong_video.mp4/playlist.m3u8?jwt=abcd", nil)
+	w := httptest.NewRecorder()
+
+	if res := validateToken(w, r); !res {
+		t.Error("validateToken returned true")
+	}
+
+	if w.Code != http.StatusOK {
+		t.Errorf("expected status code %d, got %d", http.StatusOK, w.Code)
+	}
+}
+
+// prepareJWT creates a signed JWT token and sets the edge servers public key as the key to validate the token.
+func prepareJWT(exp time.Duration, playlist string) (string, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return "", err
+	}
+	jwtPubKey = &key.PublicKey
+
+	t := jwt.New(jwt.GetSigningMethod("RS256"))
+
+	t.Claims = &JWTPlaylistClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: &jwt.NumericDate{Time: time.Now().Add(exp)},
+		},
+		UserID:   0,
+		Playlist: playlist,
+	}
+	return t.SignedString(key)
+}
