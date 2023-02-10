10c6b93a8309d8398a739576cbe95fc9b6f5678d
Khadim Fall <khadimfall0602@gmail.com>
2023-02-10 15:29:02 +0100

    revert comment out

--- web/router.go
+++ web/router.go
@@ -64,10 +64,10 @@ func configGinStaticRouter(router gin.IRoutes) {
 		router.Static("/static", "web/")
 	}
 
-	/*defaults := getDefaultStaticBrandingFiles()
+	defaults := getDefaultStaticBrandingFiles()
 	for _, file := range defaults {
 		router.GET("/"+file.Name, getFileHandler(file))
-	}*/
+	}
 
 	router.GET("/service-worker.js", func(c *gin.Context) {
 		c.FileFromFS("assets/service-worker.js", http.FS(staticFS))
