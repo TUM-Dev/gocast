64c21f6829fc22e4d4e6d0529c2b5a85d4959a86
Matthias Reumann <matthias.reumann@tum.de>
2023-03-01 12:12:16 +0100

    minor changes

---
+++ web/ts/views/home.ts
@@ -0,0 +1,93 @@
+export function pageData(): Home {
+    return {
+        showUserContext: false,
+        toggleUserContext() {
+            this.showUserContext = !this.showUserContext;
+        },
+
+        notifications: new Notifications(),
+        showNotifications: false,
+        toggleNotifications() {
+            this.showNotifications = !this.showNotifications;
+            this.notifications.writeToStorage(true);
+        },
+    };
+}
+
+interface Home {
+    showUserContext: boolean;
+    toggleUserContext();
+
+    notifications: Notifications;
+    showNotifications: boolean;
+    toggleNotifications();
+}
+
+class Notifications {
+    notifications: Notification[] = [];
+
+    constructor() {
+        this.notifications = [];
+    }
+
+    getAll(): Notification[] {
+        return this.notifications;
+    }
+
+    empty(): boolean {
+        return this.notifications.length === 0;
+    }
+
+    writeToStorage(markRead = false) {
+        if (markRead) {
+            this.notifications.forEach((notification) => {
+                notification.read = true;
+            });
+        }
+        localStorage.setItem("notifications", JSON.stringify(this.notifications));
+    }
+
+    hasNewNotifications(): boolean {
+        return this.notifications.some((notification) => !notification.read);
+    }
+
+    fetchNotifications(): void {
+        this.notifications = JSON.parse(localStorage.getItem("notifications") || "[]");
+
+        const lastNotificationFetch: Date = new Date(parseInt(localStorage.getItem("lastNotificationFetch") || "0"));
+        // fetch every 10 minutes at most:
+        if (new Date().getTime() - lastNotificationFetch.getTime() > 1000 * 60 * 10) {
+            fetch(`/api/notifications/`)
+                .then((response) => response.json() as Promise<Notification[]>)
+                .then((data) => {
+                    // merge new notifications read status with existing ones:
+                    for (let i = 0; i < this.notifications.length; i++) {
+                        for (let j = 0; j < data.length; j++) {
+                            if (data[j].id === this.notifications[i].id) {
+                                data[j].read = this.notifications[i].read;
+                                break;
+                            }
+                        }
+                    }
+                    this.notifications = data;
+                    this.writeToStorage();
+                    localStorage.setItem("lastNotificationFetch", new Date().getTime().toString());
+                });
+        }
+    }
+}
+
+export class Notification {
+    id: number;
+    createdAt: Date;
+    title: string | undefined;
+    body: string;
+    read: boolean;
+    target: number;
+
+    constructor(title: string | undefined, body: string, target: number) {
+        this.title = title;
+        this.body = body;
+        this.target = target;
+    }
+}
