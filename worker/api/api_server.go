d5fb92c9dd8017588818e54c4280c251118bfa53
Alexander Stephan <alexander.stephan@tum.de>
2023-03-28 20:37:42 +0200

    Remove hardcoded value

--- worker/api/api_server.go
+++ worker/api/api_server.go
@@ -82,7 +82,7 @@ func (s server) GenerateLiveThumbs(ctx context.Context, request *pb.LiveThumbReq
 	cmd := exec.Command("sh", "-c",
 		"ffmpeg",
 		"-sseof", "-3",
-		"-i", fmt.Sprintf("%s?jwt=%s", "http://tum-live:8089/vod/aa_2023_03_28_10_00COMB.mp4/playlist.m3u8", cfg.AdminToken),
+		"-i", fmt.Sprintf("%s?jwt=%s", request.HLSUrl, cfg.AdminToken),
 		"-vframes", "1",
 		"-update", "1",
 		"-q:v", "1",
