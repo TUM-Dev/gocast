d7ef64e75c668868b7755dd284cad636b93abb6c
matthias <matthias.reumann@tum.de>
2023-05-30 07:23:09 +0200

    Make rightclick work properly (#1048)

--- web/ts/api/courses.ts
+++ web/ts/api/courses.ts
@@ -109,7 +109,7 @@ export class Course {
     }
 
     public URL(): string {
-        return `/course/${this.Year}/${this.TeachingTerm}/${this.Slug}`;
+        return `?year=${this.Year}&term=${this.TeachingTerm}&slug=${this.Slug}&view=3`;
     }
 
     public LastRecordingURL(): string {
