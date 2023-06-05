83504fb945550ba434ed82aa7b9ef546e0034978
matthias <matthias.reumann@tum.de>
2023-06-05 08:33:07 +0200

    Preload next `N` thumbnails on new start page (#1050)
    * Add preloader
    
    * Clean up; Add fetch to course page
    
    * Change css class
    
    * Add sorting function as parameter to preload
    
    * Add null check

--- web/ts/utilities/paginator.ts
+++ web/ts/utilities/paginator.ts
@@ -4,33 +4,37 @@ export class Paginator<T> {
 
     private index: number;
 
-    constructor(list: T[], split_number: number) {
+    private readonly preloader: Preload<T>;
+
+    constructor(list: T[], split_number: number, preloader?: Preload<T>) {
         this.list = list;
         this.split_number = split_number;
         this.index = 1;
+        this.preloader = preloader;
     }
 
-    get(sortFn?: (a: T, b: T) => number, filterPred?: (o: T) => boolean): T[] {
+    get(sortFn?: CompareFunction<T>, filterPred?: FilterPredicate<T>): T[] {
         const copy = filterPred ? [...this.list].filter(filterPred) : [...this.list];
         return sortFn
             ? copy.sort(sortFn).slice(0, this.index * this.split_number)
             : copy.slice(0, this.index * this.split_number);
     }
 
-    set(list: T[]): Paginator<any> {
+    set(list: T[]): Paginator<T> {
         this.list = list;
         return this;
     }
 
     next(all = false) {
         this.index = all ? this.list.length / this.split_number : this.index + 1;
+        this.preload();
     }
 
     hasNext() {
         return Math.ceil(this.list.length / this.split_number) >= this.index + 1;
     }
 
-    forEach(callback: (obj: T, i: number) => void): Paginator<any> {
+    forEach(callback: (obj: T, i: number) => void): Paginator<T> {
         this.list.forEach(callback);
         return this;
     }
@@ -39,8 +43,24 @@ export class Paginator<T> {
         return this.list.length > 0;
     }
 
-    reset(): Paginator<any> {
+    reset(): Paginator<T> {
         this.index = 1;
         return this;
     }
+
+    preload(sortFn?: CompareFunction<T>): Paginator<T> {
+        const copy = [...this.list];
+        if (this.hasNext() && this.preloader) {
+            (sortFn ? copy.sort(sortFn) : copy)
+                .sort(sortFn)
+                .slice(this.index * this.split_number, (this.index + 1) * this.split_number)
+                .forEach((el: T) => this.preloader(el));
+        }
+        return this;
+    }
 }
+
+type Preload<T> = (o: T) => void;
+
+type CompareFunction<T> = (a: T, b: T) => number;
+type FilterPredicate<T> = (o: T) => boolean;
