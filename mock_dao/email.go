fac6e696f1ccf44605a444ebfc7f5a78dc399fb3
Joscha Henningsen <joscha.henningsen@tum.de>
2023-06-07 11:43:43 +0200

    reegn mock

--- mock_dao/email.go
+++ mock_dao/email.go
@@ -93,6 +93,21 @@ func (mr *MockEmailDaoMockRecorder) GetDue(arg0, arg1 interface{}) *gomock.Call 
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDue", reflect.TypeOf((*MockEmailDao)(nil).GetDue), arg0, arg1)
 }
 
+// GetFailed mocks base method.
+func (m *MockEmailDao) GetFailed(arg0 context.Context) ([]model.Email, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetFailed", arg0)
+	ret0, _ := ret[0].([]model.Email)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetFailed indicates an expected call of GetFailed.
+func (mr *MockEmailDaoMockRecorder) GetFailed(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetFailed", reflect.TypeOf((*MockEmailDao)(nil).GetFailed), arg0)
+}
+
 // Save mocks base method.
 func (m *MockEmailDao) Save(arg0 context.Context, arg1 *model.Email) error {
 	m.ctrl.T.Helper()
