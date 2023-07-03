a64fa3f4768423538574705a93c511ee291075c2
Khadim <khadimfall0602@gmail.com>
2023-07-03 17:18:09 +0200

    joining and giving permission is working

--- web/ts/livestream.ts
+++ web/ts/livestream.ts
@@ -1,4 +1,5 @@
 import {
+    Track,
     Participant,
     RemoteParticipant,
     RemoteTrack,
@@ -12,6 +13,10 @@ export class Livestream {
     private streamId: number;
     private url: string;
     private token: string;
+    private room: Room;
+    private screenDisplay: HTMLElement;
+    private camDisplay: HTMLElement;
+
 
     constructor(streamId: number, url: string, token: string) {
         this.streamId = streamId;
@@ -20,8 +25,10 @@ export class Livestream {
         console.log("init", streamId);
     }
 
-    public async connect() {
-        const room = new Room({
+    public async connect(screenDisplay: HTMLElement, camDisplay: HTMLElement) {
+        this.screenDisplay = screenDisplay;
+        this.camDisplay = camDisplay;
+        this.room = new Room({
             adaptiveStream: true,
             dynacast: true,
             videoCaptureDefaults: {
@@ -29,7 +36,40 @@ export class Livestream {
             },
         });
 
-        await room.connect(this.url, this.token);
-        console.log('connected to room', room.name);
+        await this.room.connect(this.url, this.token);
+        console.log('connected to room', this.room.name);
+    }
+
+    public async enableScreen() {
+        await this.room.localParticipant.setScreenShareEnabled(true);
+        this.updateLocal();
+    }
+
+    public async enableCam() {
+        await this.room.localParticipant.enableCameraAndMicrophone();
+        this.updateLocal();
+    }
+
+    private updateLocal() {
+        const tracks = this.room.localParticipant.videoTracks;
+        tracks.forEach(({track, source}) => {
+            console.log(track);
+            if (source == Track.Source.ScreenShare) {
+                const element = track.attach();
+                element.style.width = "100%";
+                element.style.height = "100%";
+
+                this.screenDisplay.innerHTML = "";
+                this.screenDisplay.appendChild(element);
+            } else if (source == Track.Source.Camera) {
+                const element = track.attach();
+                element.style.width = "100%";
+                element.style.height = "100%";
+
+                this.camDisplay.innerHTML = "";
+                this.camDisplay.appendChild(element);
+                console.log(this.camDisplay, element);
+            }
+        })
     }
 }
