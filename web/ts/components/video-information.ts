c7375241f912f65e8bad8fda44643dc48c8b164c
Matthias Reumann <matthias.reumann@tum.de>
2023-07-03 07:57:12 +0200

    Update description properly

--- web/ts/components/video-information.ts
+++ web/ts/components/video-information.ts
@@ -1,12 +1,25 @@
 import { AlpineComponent } from "./alpine-component";
 import { SocketConnections } from "../api/chat-ws";
+import { ToggleableElement } from "../utilities/ToggleableElement";
+
+const CUTOFFLENGTH = 256;
 
 export function videoInformationContext(): AlpineComponent {
+    // TODO: REST
+    const descriptionEl = document.getElementById("description") as HTMLInputElement;
     return {
         viewers: 0 as number,
+        description: descriptionEl.value as string,
+        less: descriptionEl.value.length > CUTOFFLENGTH,
+
+        showFullDescription: new ToggleableElement(),
 
         init() {
             Promise.all([this.initWebsocket()]);
+        },
+
+        hasDescription(): boolean {
+            return this.description.length > 0;
         },
 
         async initWebsocket() {
@@ -15,7 +28,7 @@ export function videoInformationContext(): AlpineComponent {
                 if ("viewers" in data) {
                     this.handleViewersUpdate(data);
                 } else if ("description" in data) {
-                    console.log(data);
+                    this.handleDescriptionUpdate(data);
                 }
             };
             SocketConnections.ws.addHandler(handler);
@@ -23,6 +36,11 @@ export function videoInformationContext(): AlpineComponent {
 
         handleViewersUpdate(upd: { viewers: number }) {
             this.viewers = upd.viewers;
+        },
+
+        handleDescriptionUpdate(upd: { description: { full: string } }) {
+            this.less = upd.description.full.length > CUTOFFLENGTH;
+            this.description = upd.description.full;
         },
     } as AlpineComponent;
 }
