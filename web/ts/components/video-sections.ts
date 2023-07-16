8678c3ffe3a02de0ea819502b84cbb5f98b6acd3
Matthias Reumann <matthias.reumann@tum.de>
2023-07-16 15:31:25 +0200

    Update css

--- web/ts/components/video-sections.ts
+++ web/ts/components/video-sections.ts
@@ -5,15 +5,27 @@ import { SlidingWindow } from "../utilities/sliding-window";
 import { registerTimeWatcher } from "../video/watchers";
 import { getPlayers } from "../TUMLiveVjs";
 import { Time } from "../utilities/time";
+import { ToggleableElement } from "../utilities/ToggleableElement";
 
 export function videoSectionContext(streamId: number): AlpineComponent {
     return {
         streamId: streamId,
+        autoScroll: new ToggleableElement(),
         sections: new SlidingWindow([], 6),
 
         init() {
             DataStore.videoSections.subscribe(this.streamId, this.updateSection.bind(this));
             registerTimeWatcher(getPlayers()[0], this.setCurrent.bind(this));
+        },
+
+        nextSection() {
+            this.autoScroll.toggle(false);
+            this.sections.next();
+        },
+
+        prevSection() {
+            this.autoScroll.toggle(false);
+            this.sections.prev();
         },
 
         setCurrent(t: number) {
@@ -29,7 +41,7 @@ export function videoSectionContext(streamId: number): AlpineComponent {
 
             if (section) {
                 section.isCurrent = true;
-                if (!this.sections.isInWindow(section)) this.sections.show(section);
+                if (!this.sections.isInWindow(section) && this.autoScroll.value) this.sections.show(section);
             }
         },
 
