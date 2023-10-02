00cd3d15562087058150c7aedcc54017bfeb81e2
Joscha Henningsen <joscha.henningsen@tum.de>
2023-10-02 07:38:58 +0200

    start new runner implementation

---
+++ api/runner_grpc.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"context"
+	"database/sql"
+	"github.com/TUM-Dev/gocast/dao"
+	"github.com/TUM-Dev/gocast/model"
+	log "github.com/sirupsen/logrus"
+	"github.com/tum-dev/gocast/runner/protobuf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"time"
+)
+
+var _ protobuf.FromRunnerServer = (*GrpcRunnerServer)(nil)
+
+type GrpcRunnerServer struct {
+	protobuf.UnimplementedFromRunnerServer
+
+	dao.DaoWrapper
+}
+
+func (g GrpcRunnerServer) Register(ctx context.Context, request *protobuf.RegisterRequest) (*protobuf.RegisterResponse, error) {
+	runner := model.Runner{
+		Hostname: request.Hostname,
+		Port:     int(request.Port),
+		LastSeen: sql.NullTime{Valid: true, Time: time.Now()},
+	}
+	err := g.RunnerDao.Create(ctx, &runner)
+	if err != nil {
+		return nil, err
+	}
+	return &protobuf.RegisterResponse{}, nil
+}
+
+func (g GrpcRunnerServer) Heartbeat(ctx context.Context, request *protobuf.HeartbeatRequest) (*protobuf.HeartbeatResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (g GrpcRunnerServer) RequestSelfStream(ctx context.Context, request *protobuf.SelfStreamRequest) (*protobuf.SelfStreamResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (g GrpcRunnerServer) mustEmbedUnimplementedFromRunnerServer() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func StartGrpcRunnerServer() {
+	lis, err := net.Listen("tcp", ":50056")
+	if err != nil {
+		log.WithError(err).Error("Failed to init grpc server")
+		return
+	}
+	grpcServer := grpc.NewServer(grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle:     time.Minute,
+		MaxConnectionAge:      time.Minute * 5,
+		MaxConnectionAgeGrace: time.Second * 5,
+		Time:                  time.Minute * 10,
+		Timeout:               time.Second * 20,
+	}))
+	protobuf.RegisterFromRunnerServer(grpcServer, &GrpcRunnerServer{DaoWrapper: dao.NewDaoWrapper()})
+	reflection.Register(grpcServer)
+	go func() {
+		if err = grpcServer.Serve(lis); err != nil {
+			log.WithError(err).Errorf("Can't serve grpc")
+		}
+	}()
+}
