25b54911ed9e9da3dd041d5b1412730434ed1625
Dawin Yurtseven <dawinyurtseven@gmail.com>
2023-10-18 15:25:09 +0200

    Reasons for the BeforeUnload events:
    - The import body was a form without a submit, making the webbrowser think that changes have been made without saving. this has been fixed by making the body a <div> instead of a <form>
    - The EventListener in course-import.ts hasn't been removed even after import has been finished, making the browser ask even when import has been finished. to fix this, I wrote the function separately to first add it during the first step in "notify1" and then remove it in "notify2" when the process is finished.

--- web/ts/course-import.ts
+++ web/ts/course-import.ts
@@ -14,30 +14,38 @@ export function pageData() {
     return d;
 }
 
+// basic function that warns users when leaving without changes
+function onBeforeUnloadHandle(e) {
+    e.preventDefault();
+    e.returnValue = "";
+}
+
 // lecture hall selected -> api call
 export function addNotifyEventListeners() {
     window.addEventListener("notify1", () => {
-        // warn users when leaving site:
-        window.addEventListener("beforeunload", function (e) {
-            e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
-            // Chrome requires returnValue to be set
-            e.returnValue = "";
-        });
+
+        // warn users when leaving site using the given function:
+        window.addEventListener("beforeunload", onBeforeUnloadHandle);
+
         window.dispatchEvent(new CustomEvent("loading-start"));
         fetch(`/api/course-schedule?range=${d.range}&department=${d.department}&departmentID=${d.departmentID}`).then(
             (res) => {
                 res.text().then((text) => {
                     console.log(text);
-                    window.dispatchEvent(new CustomEvent("loading-end", { detail: { courses: JSON.parse(text) } }));
+                    window.dispatchEvent(new CustomEvent("loading-end", {detail: {courses: JSON.parse(text)}}));
                 });
             },
         );
     });
+
     window.addEventListener("notify2", () => {
+        //Removes the eventListener so that after import since user won't change anything no longer
+        window.removeEventListener("beforeunload", onBeforeUnloadHandle);
+
         fetch(`/api/course-schedule/${d.year}/${d.semester}`, {
             method: "POST",
-            body: JSON.stringify({ courses: d.courses, optIn: d.optInOut === "Opt In" }),
-        }).then((r) => window.dispatchEvent(new CustomEvent("imported", { detail: r.status })));
+            body: JSON.stringify({courses: d.courses, optIn: d.optInOut === "Opt In"}),
+        }).then((r) => window.dispatchEvent(new CustomEvent("imported", {detail: r.status})));
     });
 
     window.addEventListener("notify3", () => {
