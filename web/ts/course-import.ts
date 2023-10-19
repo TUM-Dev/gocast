464d16510e7bd8e147ef27a599fe8e427a02aedd
Dawin Yurtseven <dawinyurtseven@gmail.com>
2023-10-19 12:58:30 +0200

    executed "npm run lint-fix" to fix ESLint fails

--- web/ts/course-import.ts
+++ web/ts/course-import.ts
@@ -23,7 +23,6 @@ function onBeforeUnloadHandle(e) {
 // lecture hall selected -> api call
 export function addNotifyEventListeners() {
     window.addEventListener("notify1", () => {
-
         // warn users when leaving site using the given function:
         window.addEventListener("beforeunload", onBeforeUnloadHandle);
 
@@ -32,7 +31,7 @@ export function addNotifyEventListeners() {
             (res) => {
                 res.text().then((text) => {
                     console.log(text);
-                    window.dispatchEvent(new CustomEvent("loading-end", {detail: {courses: JSON.parse(text)}}));
+                    window.dispatchEvent(new CustomEvent("loading-end", { detail: { courses: JSON.parse(text) } }));
                 });
             },
         );
@@ -44,8 +43,8 @@ export function addNotifyEventListeners() {
 
         fetch(`/api/course-schedule/${d.year}/${d.semester}`, {
             method: "POST",
-            body: JSON.stringify({courses: d.courses, optIn: d.optInOut === "Opt In"}),
-        }).then((r) => window.dispatchEvent(new CustomEvent("imported", {detail: r.status})));
+            body: JSON.stringify({ courses: d.courses, optIn: d.optInOut === "Opt In" }),
+        }).then((r) => window.dispatchEvent(new CustomEvent("imported", { detail: r.status })));
     });
 
     window.addEventListener("notify3", () => {
