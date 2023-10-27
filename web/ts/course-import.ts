91ffa7477732b5b52e96f034e535d5bd08afec29
Dawin Yurtseven <dawinyurtseven@gmail.com>
2023-10-27 12:24:45 +0200

    added comments to newly added function. Will plan on adding more comments to the other functions for better readability later on.

--- web/ts/course-import.ts
+++ web/ts/course-import.ts
@@ -14,11 +14,20 @@ export function pageData() {
     return d;
 }
 
-// basic function that warns users when leaving without changes
+/**
+ * onBeforeUnloadHandle is a function that will create a prompt asking the user if they want to leave
+ *
+ * The reason this has been made a separate Function and not in the "addEventListener" is because
+ * the function has to be taken out in the EventListener so that after the import has been done, the
+ * user does not need to be asked if they wanted to leave
+ * @param e is just the event that it will accept from the BeforeUnload event
+ */
+
 function onBeforeUnloadHandle(e) {
-    e.preventDefault();
-    e.returnValue = "";
+    e.preventDefault(); // If you prevent default behavior in Mozilla Firefox, prompt will always be shown
+    e.returnValue = ""; //Chrome requires returnValue to be set
 }
+
 
 // lecture hall selected -> api call
 export function addNotifyEventListeners() {
@@ -38,13 +47,14 @@ export function addNotifyEventListeners() {
     });
 
     window.addEventListener("notify2", () => {
-        //Removes the eventListener after import has been done since user won't change anything no longer
-        window.removeEventListener("beforeunload", onBeforeUnloadHandle);
 
         fetch(`/api/course-schedule/${d.year}/${d.semester}`, {
             method: "POST",
             body: JSON.stringify({ courses: d.courses, optIn: d.optInOut === "Opt In" }),
         }).then((r) => window.dispatchEvent(new CustomEvent("imported", { detail: r.status })));
+
+        //Removes the eventListener after import has been done since user won't change anything no longer
+        window.removeEventListener("beforeunload", onBeforeUnloadHandle);
     });
 
     window.addEventListener("notify3", () => {
