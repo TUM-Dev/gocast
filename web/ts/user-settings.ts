e7523a0060ffba50b0fd72db45238c4af5260923
Sebastian <sebiwrn@web.de>
2023-12-07 16:48:04 +0100

    Fixed a bug

--- web/ts/user-settings.ts
+++ web/ts/user-settings.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from "http-status-codes";
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 const settingsAPIBaseURL = "/api/users/settings";
 
@@ -8,7 +8,6 @@ export enum UserSetting {
     Greeting = "greeting",
     PlaybackSpeeds = "playbackSpeeds",
     CustomSpeeds = "customSpeeds",
-
 }
 
 export function updatePreference(t: UserSetting, value: string | boolean | number[]): Promise<string> {
@@ -27,17 +26,16 @@ export function updatePreference(t: UserSetting, value: string | boolean | numbe
     });
 }
 
-export function sanitizeInputSpeed(value: number) : number {
+export function sanitizeInputSpeed(value: number): number {
     if (value > 5) {
         return 5;
     } else if (value <= 0) {
         return 0.01;
     }
-    return Math.round((value) * 100) / 100
+    return Math.round(value * 100) / 100;
 }
 
 export function checkInputSpeed(value: number, currentSpeeds: number[]) {
-    const defaultSpeeds = [.25, .5, .75, 1, 1.5, 1.5, 1.75, 2, 2.5, 3, 3.5];
-    return (!defaultSpeeds.includes(value)) && (!currentSpeeds.includes(value));
-
+    const defaultSpeeds = [0.25, 0.5, 0.75, 1, 1.5, 1.5, 1.75, 2, 2.5, 3, 3.5];
+    return !defaultSpeeds.includes(value) && !currentSpeeds.includes(value);
 }
