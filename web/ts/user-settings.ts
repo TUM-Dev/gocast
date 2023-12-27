d7f3f491622d124dab3001d3cffe4aff26221f59
Sebastian <sebiwrn@web.de>
2023-12-27 16:32:31 +0100

    Lint fix

--- web/ts/user-settings.ts
+++ web/ts/user-settings.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from "http-status-codes";
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 const settingsAPIBaseURL = "/api/users/settings";
 
@@ -26,16 +26,16 @@ export function updatePreference(t: UserSetting, value: string | boolean | numbe
     });
 }
 
-export function sanitizeInputSpeed(value: number) : number {
+export function sanitizeInputSpeed(value: number): number {
     if (value > 5) {
         return 5;
     } else if (value <= 0) {
         return 0.01;
     }
-    return Math.round((value) * 100) / 100
+    return Math.round(value * 100) / 100;
 }
 
 export function checkInputSpeed(value: number, currentSpeeds: number[]) {
-    const defaultSpeeds = [.25, .5, .75, 1, 1.5, 1.5, 1.75, 2, 2.5, 3, 3.5];
-    return (!defaultSpeeds.includes(value)) && (!currentSpeeds.includes(value) && currentSpeeds.length < 3);
+    const defaultSpeeds = [0.25, 0.5, 0.75, 1, 1.5, 1.5, 1.75, 2, 2.5, 3, 3.5];
+    return !defaultSpeeds.includes(value) && !currentSpeeds.includes(value) && currentSpeeds.length < 3;
 }
