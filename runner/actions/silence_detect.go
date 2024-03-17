021d3805c6aefaca64be6df171545b3384288f09
meandaD <yiran.duan@tum.de>
2024-03-18 00:17:38 +0100

    send silence results to server on success

--- runner/actions/silence_detect.go
+++ runner/actions/silence_detect.go
@@ -3,6 +3,7 @@ package actions
 import (
 	"context"
 	"fmt"
+	"github.com/tum-dev/gocast/runner/protobuf"
 	"log/slog"
 	"os/exec"
 	"strconv"
@@ -13,6 +14,11 @@ func (a *ActionProvider) SilenceDetectAction() *Action {
 	return &Action{
 		Type: SilenceDetectAction,
 		ActionFn: func(ctx context.Context, log *slog.Logger) (context.Context, error) {
+			streamID, ok := ctx.Value("stream").(uint64)
+			if !ok {
+				return ctx, fmt.Errorf("%w: context doesn't contain stream", ErrRequiredContextValNotFound)
+			}
+
 			filename, ok := ctx.Value("outputFilename").(string)
 			if !ok {
 				errMsg := "no transcoded file to process for silence detection"
@@ -35,9 +41,14 @@ func (a *ActionProvider) SilenceDetectAction() *Action {
 				return ctx, err
 			}
 
-			silences = postprocess(silences)
-			// TODO: send the results to TUM-Live server, instead of just logging them
+			starts, ends := postprocess(silences)
 			log.Info("Silences detected", "file", filename, "silences", silences)
+			a.Server.NotifySilenceResults(ctx, &protobuf.SilenceResults{
+				RunnerID: "0", // TODO: replace with runner ID
+				StreamID: uint32(streamID),
+				Starts:   starts,
+				Ends:     ends,
+			})
 
 			return ctx, nil
 		},
@@ -54,7 +65,7 @@ func parseSilence(output string) ([]silence, error) {
 				return nil, err
 			}
 			silences = append(silences, silence{
-				Start: uint(start),
+				Start: uint32(start),
 				End:   0,
 			})
 		} else if strings.Contains(line, "silence_end:") {
@@ -62,33 +73,42 @@ func parseSilence(output string) ([]silence, error) {
 			if err != nil || silences == nil || len(silences) == 0 {
 				return nil, err
 			}
-			silences[len(silences)-1].End = uint(end)
+			silences[len(silences)-1].End = uint32(end)
 		}
 	}
 	return silences, nil
 }
 
-// postprocess merges short durations of silence into units of silence
-func postprocess(silences []silence) []silence {
-	if len(silences) < 2 {
-		return silences
-	}
-	if silences[0].Start < 30 {
-		silences[0].Start = 0
-	}
-	var newSilences []silence
-	newSilences = append(newSilences, silences[0])
-	for i := 1; i < len(silences); i++ {
-		if silences[i].Start-newSilences[len(newSilences)-1].End < 30 {
-			newSilences[len(newSilences)-1].End = silences[i].End
-		} else {
-			newSilences = append(newSilences, silences[i])
+// postprocess merges short durations of silence into units of silence,
+// and returns starts and ends as two separate arrays
+func postprocess(silences []silence) ([]uint32, []uint32) {
+	if len(silences) >= 2 {
+		if silences[0].Start < 30 {
+			silences[0].Start = 0
 		}
+		var newSilences []silence
+		newSilences = append(newSilences, silences[0])
+		for i := 1; i < len(silences); i++ {
+			if silences[i].Start-newSilences[len(newSilences)-1].End < 30 {
+				newSilences[len(newSilences)-1].End = silences[i].End
+			} else {
+				newSilences = append(newSilences, silences[i])
+			}
+		}
+		silences = newSilences
 	}
-	return newSilences
+
+	var starts []uint32
+	var ends []uint32
+	for i := 1; i < len(silences); i++ {
+		starts = append(starts, silences[i].Start)
+		ends = append(ends, silences[i].End)
+	}
+
+	return starts, ends
 }
 
 type silence struct {
-	Start uint
-	End   uint
+	Start uint32
+	End   uint32
 }
