fc5cadc641ac13bca943ef1773ca434660acc75b
Sebastian <sebiwrn@web.de>
2024-04-25 19:25:34 +0200

    Fixed some things and cleaned up code

--- api/progress.go
+++ api/progress.go
@@ -117,36 +117,36 @@ func (r progressRoutes) saveProgress(c *gin.Context) {
 		return
 	}
 
-	streamIDString := strconv.FormatUint(uint64(request.StreamID), 10)
-	if r.StreamsDao == nil {
-		logger.Error("StreamsDao is nil")
-		return
-	}
-	stream, err := r.StreamsDao.GetStreamByID(c, streamIDString)
+	stream := tumLiveContext.Stream
 
-	if err == nil {
-		lastSilence := slices.MaxFunc(stream.Silences, func(silence model.Silence, other model.Silence) int {
-			return int(silence.End) - int(other.End)
-		})
+	var watchedToLastSilence = false
 
-		if math.Abs(float64(lastSilence.End-uint(stream.Duration.Int32))) < 10 {
-			lastSilencePercent := float64(lastSilence.Start) / float64(stream.Duration.Int32)
-			if request.Progress >= lastSilencePercent {
-				progressBuff.add(model.StreamProgress{
-					Progress: request.Progress,
-					StreamID: request.StreamID,
-					UserID:   tumLiveContext.User.ID,
-					Watched:  true,
-				})
-				return
+	if stream != nil {
+		duration := stream.Duration.Int32
+		if duration == 0 {
+			dur := stream.End.Sub(stream.Start)
+			duration += int32(dur.Seconds()) + int32(dur.Minutes())*60 + int32(dur.Minutes())*60*60
+		}
+		if duration != 0 {
+			lastSilence := slices.MaxFunc(stream.Silences, func(silence model.Silence, other model.Silence) int {
+				return int(silence.End) - int(other.End)
+			})
+
+			// Add a little wiggle time to the end if ffmpeg didn't detect the silence till the end
+			if math.Abs(float64(lastSilence.End-uint(stream.Duration.Int32))) < 10 {
+				lastSilencePercent := float64(lastSilence.Start) / float64(stream.Duration.Int32)
+				if request.Progress >= lastSilencePercent {
+					watchedToLastSilence = true
+				}
 			}
 		}
+
 	}
 	progressBuff.add(model.StreamProgress{
 		Progress: request.Progress,
 		StreamID: request.StreamID,
 		UserID:   tumLiveContext.User.ID,
-		Watched:  request.Progress > .9,
+		Watched:  request.Progress > .9 || watchedToLastSilence,
 	})
 }
 
