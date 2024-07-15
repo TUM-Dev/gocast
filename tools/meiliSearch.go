19217fd6405c4aa029b76f79032b4e75e8fc910b
johanneskarrer <ge87zim@mytum.de>
2024-07-15 15:44:02 +0200

    adds missing file for search box

--- tools/meiliSearch.go
+++ tools/meiliSearch.go
@@ -2,8 +2,9 @@ package tools
 
 import (
 	"fmt"
-
 	"github.com/meilisearch/meilisearch-go"
+	"strconv"
+	"strings"
 )
 
 func SearchSubtitles(q string, streamID uint) *meilisearch.SearchResponse {
@@ -20,4 +21,37 @@ func SearchSubtitles(q string, streamID uint) *meilisearch.SearchResponse {
 		return nil
 	}
 	return response
+}
+
+func SearchCourses(q string, year int, t string, searchableCourseIDs *[]uint) *meilisearch.SearchResponse {
+	c, err := Cfg.GetMeiliClient()
+	if err != nil {
+		return nil
+	}
+	if t != "W" && t != "S" {
+		return nil
+	}
+
+	courseIdFilter := courseIdFilter(searchableCourseIDs)
+	response, err := c.Index("COURSES").Search(q, &meilisearch.SearchRequest{
+		Filter: fmt.Sprintf("year = %d AND teachingTerm = %s AND ID IN %s", year, t, courseIdFilter),
+		Limit:  10,
+	})
+
+	if err != nil {
+		logger.Error("could not search courses in meili", "err", err)
+		return nil
+	}
+	return response
+}
+
+// returns a string conforming to Meili Search Filters Format containing each courseId passed onto the function
+func courseIdFilter(searchableCourseIDs *[]uint) string {
+	var courseIDsAsStringArray []string
+	courseIDsAsStringArray = make([]string, len(*searchableCourseIDs))
+	for i, courseID := range *searchableCourseIDs {
+		courseIDsAsStringArray[i] = strconv.FormatUint(uint64(courseID), 10)
+	}
+	courseIdFilter := "[" + strings.Join(courseIDsAsStringArray, ", ") + "]"
+	return courseIdFilter
 }
