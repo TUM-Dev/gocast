3b6f679a016154a7b7f0d81313b67f21f149f2f3
Sebastian <sebiwrn@web.de>
2024-09-11 14:31:09 +0200

    Added course filter but not finished yet

--- web/ts/search.ts
+++ web/ts/search.ts
@@ -1,4 +1,5 @@
 import {Semester} from "./api/semesters";
+import {Course, CoursesAPI} from "./api/courses";
 
 export function coursesSearch() {
     return {
@@ -39,15 +40,28 @@ export function searchPlaceholder() {
 
 function getSemestersString(years: number[], teachingTerms: string[]) : string {
     let ret = "";
-    if (years.length == 0 && teachingTerms.length == 0) {
+    if(years.length != teachingTerms.length) {
         return ret;
-    } else if (years.length == teachingTerms.length) {
-        for (let i = 0; i < years.length; i++) {
-            if(i == years.length - 1) {
-                ret += years[i] + teachingTerms[i];
-            } else {
-                ret += years[i] + teachingTerms[i] + ",";
-            }
+    }
+    for (let i = 0; i < years.length; i++) {
+        if(i == years.length - 1) {
+            ret += years[i] + teachingTerms[i];
+        } else {
+            ret += years[i] + teachingTerms[i] + ",";
+        }
+    }
+    return ret;
+}
+
+function getCoursesString(courses: Course[]) : string {
+    console.log("Courses are: ")
+    console.log(courses)
+    let ret = "";
+    for(let i = 0; i < courses.length; i++) {
+        if(i == courses.length - 1) {
+            ret += courses[i].Slug + courses[i].Year + courses[i].TeachingTerm;
+        } else {
+            ret += courses[i].Slug + courses[i].Year + courses[i].TeachingTerm + ",";
         }
     }
     return ret;
@@ -58,10 +72,10 @@ export function filteredSearch() {
         hits: {},
         open: false,
         searchInput: "",
-        search: function (years: number[], teachingTerms: string[], courses: string[], limit: number = 20) {
+        search: function (years: number[], teachingTerms: string[], courses: Course[], limit: number = 20) {
             if (this.searchInput.length > 2) {
                 if (years.length < 8 && teachingTerms.length < 8 && teachingTerms.length == years.length && courses.length < 2) {
-                    fetch(`/api/search?q=${this.searchInput}&semester=${years.length > 0 ? encodeURIComponent(getSemestersString(years, teachingTerms)) : ""}&course=${courses.length > 0 ? encodeURIComponent(courses.join(",")) : ""}&limit=${limit}`)
+                    fetch(`/api/search?q=${this.searchInput}&semester=${encodeURIComponent(getSemestersString(years, teachingTerms))}&course=${encodeURIComponent(getCoursesString(courses))}&limit=${limit}`)
                         .then((res) => {
                                 if (res.ok) {
                                     res.json().then((data) => {
@@ -78,7 +92,9 @@ export function filteredSearch() {
             }
 
         },
-        searchWithDataFromPage: function (semesters: Semester[], selectedSemesters: number[]) {
+        searchWithDataFromPage: function (semesters: Semester[], selectedSemesters: number[], allCourses: Course[], selectedCourses: number[]) {
+            console.log(allCourses)
+            console.log(selectedCourses)
             let years = [];
             let teachingTerms = [];
             let courses = [];
@@ -86,6 +102,9 @@ export function filteredSearch() {
             for (let i = 0; i < selectedSemesters.length; i++) {
                 years.push(semesters[selectedSemesters[i]].Year);
                 teachingTerms.push(semesters[selectedSemesters[i]].TeachingTerm);
+            }
+            for (let i = 0; i < selectedCourses.length; i++) {
+                courses.push(allCourses[selectedCourses[i]]);
             }
             this.search(years, teachingTerms, courses);
         }
@@ -156,15 +175,34 @@ export function getSearchQueryFromParam() {
     return params.get("q");
 }
 
+export function getCourseFromParam() {
+    let url = new URL(document.location.href);
+    let params = new URLSearchParams(url.search);
+    return params.get("slug");
+}
+
+export function getSemestersFromParam() {
+    let url = new URL(document.location.href);
+    let params = new URLSearchParams(url.search);
+    return params.get("semester");
+}
+
+export async function getCoursesOfSemesters(semesters: Semester[], filterSemesters: number[]) : Promise<Course[]>{
+    let courses : Course[] = [];
+    for(let i = 0; i < filterSemesters.length; i++) {
+        courses = courses.concat(await CoursesAPI.getPublic(semesters[filterSemesters[i]].Year, semesters[filterSemesters[i]].TeachingTerm));
+        courses = courses.concat(await CoursesAPI.getUsers(semesters[filterSemesters[i]].Year, semesters[filterSemesters[i]].TeachingTerm));
+    }
+
+    return [...new Set(courses)];
+}
+
 export function initSearchBarArrowKeysListener() {
     document.addEventListener("keydown", (event) => {
         if (document.getElementById("search-results") == null) {
             return;
         }
         let searchResults = document.getElementById("search-results").querySelectorAll("li[role='option']");
-        console.log(searchResults);
-
-        console.log("Keydown")
         let activeElement = document.activeElement as HTMLLIElement;
         if (event.key == "ArrowDown") {
             let currentIndex = Array.from(searchResults).indexOf(activeElement);
