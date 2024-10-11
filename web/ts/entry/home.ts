1182cf1d7292429648c5d485876cb1157d1d0125
Karakoc <ga27pan@mytum.de>
2024-10-11 10:39:43 +0200

    ESLint fixes

--- web/ts/entry/home.ts
+++ web/ts/entry/home.ts
@@ -4,4 +4,4 @@ export * from "../components/livestreams";
 export * from "../components/course";
 export * from "../components/servernotifications";
 export * from "../components/main";
-export * from "../utilities/lectureHallValidator"
+export * from "../utilities/lectureHallValidator";
