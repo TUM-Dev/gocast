878fec406204157095ddc4a837d8cf80f76d2864
Sebastian <sebiwrn@web.de>
2024-11-12 16:41:29 +0100

    Added reactions to watch page

--- web/ts/api/stream-reactions.ts
+++ web/ts/api/stream-reactions.ts
@@ -1,13 +1,15 @@
 import {getData, postData} from "../global";
+import {get} from "../utilities/fetch-wrappers";
 
 
 // Function to add a reaction to a stream
 export function addReaction(reaction: string, streamID: number) {
-    return postData(`/api/${streamID}/reactions`, { reaction: reaction });
+    return postData(`/api/stream/${streamID}/reaction`, { reaction: reaction });
 }
 
-export function getAllowedReactions(streamID: number) {
-    return getData(`/api/${streamID}/reactions/allowed`).then((data) => {
+// Function to get all possible reactions for a stream
+export function getAllowedReactions(streamID: number): Promise<string[]> {
+    return get(`/api/stream/${streamID}/reaction/allowed`).then((data) => {
         return data;
     });
 }
