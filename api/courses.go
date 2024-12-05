236d56414e38207697e17ef7e9cfa8ea422d90dd
Kemal Karakoç <42945832+KemalKrKX@users.noreply.github.com>
2024-12-06 00:08:24 +0100

    Update courses.go

--- api/courses.go
+++ api/courses.go
@@ -288,10 +288,10 @@ func (r coursesRoutes) getPinned(c *gin.Context) {
 	for _, course := range pinnedCourses {
 		// todo: check if user is allowed to see the course
 		// -> Get full course from database
-		enrolled, err := r.CoursesDao.IsUserEnrolledInCourse(tumLiveContext.User.ID, course.ID)
-		if err != nil {
-			enrolled = false
-		}
+		//enrolled, err := r.CoursesDao.IsUserEnrolledInCourse(tumLiveContext.User.ID, course.ID)
+		//if err != nil {
+			//enrolled = false
+		//}
 		if course.Visibility == "enrolled" && !enrolled {
 			continue
 		}
