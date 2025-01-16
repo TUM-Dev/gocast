db7332c2876924ad7f5b8414cc62fca38fa2b581
carlobortolan <carlobortolan@gmail.com>
2025-01-16 02:02:22 +0100

    Update stream queries to filter by course year and hash user ID for test course slug

--- dao/streams.go
+++ dao/streams.go
@@ -2,6 +2,8 @@ package dao
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"strconv"
 	"strings"
@@ -331,8 +333,8 @@ func (d streamsDao) GetSoonStartingStreamInfo(user *model.User, slug string, yea
 		Select("streams.course_id, streams.stream_key, streams.id, courses.slug, streams.start, streams.end").
 		Joins("JOIN course_admins ON course_admins.course_id = streams.course_id").
 		Joins("JOIN courses ON courses.id = course_admins.course_id").
-		Where("courses.slug != 'TESTCOURSE' AND streams.deleted_at IS NULL AND courses.deleted_at IS NULL AND course_admins.user_id = ? AND (streams.start <= ? AND streams.end >= ?)", user.ID, now.Add(15*time.Minute), now). // Streams starting in the next 15 minutes or currently running
-		Or("courses.slug != 'TESTCOURSE' AND streams.deleted_at IS NULL AND courses.deleted_at IS NULL AND course_admins.user_id = ? AND (streams.end >= ? AND streams.end <= ?)", user.ID, now.Add(-15*time.Minute), now)      // Streams that just finished in the last 15 minutes
+		Where("courses.year != 1234 AND streams.deleted_at IS NULL AND courses.deleted_at IS NULL AND course_admins.user_id = ? AND (streams.start <= ? AND streams.end >= ?)", user.ID, now.Add(15*time.Minute), now). // Streams starting in the next 15 minutes or currently running
+		Or("courses.year != 1234 AND streams.deleted_at IS NULL AND courses.deleted_at IS NULL AND course_admins.user_id = ? AND (streams.end >= ? AND streams.end <= ?)", user.ID, now.Add(-15*time.Minute), now)      // Streams that just finished in the last 15 minutes
 
 	if slug != "" {
 		query = query.Where("courses.slug = ?", slug)
@@ -411,10 +413,15 @@ func (d streamsDao) CreateOrGetTestCourse(user *model.User) (model.Course, error
 		userName += "'s "
 	}
 
+	// Hash the user ID to create a unique slug withouth exposing the user ID
+	hasher := sha256.New()
+	hasher.Write([]byte(fmt.Sprintf("%d", user.ID)))
+	hashedUserID := hex.EncodeToString(hasher.Sum(nil))
+
 	err := DB.FirstOrCreate(&course, model.Course{
 		Name:         userName + "Test Course",
 		TeachingTerm: "W",
-		Slug:         "TESTCOURSE",
+		Slug:         "TEST-" + hashedUserID,
 		Year:         1234,
 		Visibility:   "hidden",
 		VODEnabled:   false, // TODO: Change to VODEnabled: true for default testcourse if necessary
