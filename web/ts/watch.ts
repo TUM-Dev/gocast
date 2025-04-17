c1798625b1abe7f5b4980fea7a83ac50de85b8e1
Sebastian <sebiwrn@web.de>
2025-04-17 16:27:14 +0200

    eslint fix 2

--- web/ts/watch.ts
+++ web/ts/watch.ts
@@ -195,7 +195,7 @@ export function setHighestQuality() {
             console.debug("Quality Level changed!");
             console.debug("New level:", qualityLevels[qualityLevels.selectedIndex]);
         });
-        qualityLevels.trigger({type: "change", selectedIndex: highestQuality});
+        qualityLevels.trigger({ type: "change", selectedIndex: highestQuality });
         qualityLevels.selectedIndex_ = highestQuality;
         for (let i = 0; i < qualityLevels.length; i++) {
             qualityLevels[i].enabled = i == highestQuality;
