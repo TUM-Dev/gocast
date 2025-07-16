e98fd0bf05def0bf2903156f0e938e7528bb7fd0
carlobortolan <carlobortolan@gmail.com>
2025-07-16 21:04:39 +0200

    fix: LoadPinnedCourses

--- web/index.go
+++ web/index.go
@@ -253,11 +253,7 @@ func (d *IndexData) LoadPinnedCourses() {
 	if d.TUMLiveContext.User != nil {
 		pinnedCourses = d.TUMLiveContext.User.PinnedCourses
 		for i := range pinnedCourses {
-			if d.TUMLiveContext.User.IsEligibleToSearchForCourse(pinnedCourses[i]) {
-				pinnedCourses[i].Pinned = false
-			} else {
-				pinnedCourses[i].Pinned = true
-			}
+			pinnedCourses[i].Pinned = d.TUMLiveContext.User.IsEligibleToSearchForCourse(pinnedCourses[i])
 		}
 		sortCourses(pinnedCourses)
 		d.PinnedCourses = commons.Unique(pinnedCourses, func(c model.Course) uint { return c.ID })
