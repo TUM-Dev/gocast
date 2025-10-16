515502a3c9543ba42195b52537f90fb3bf3e54c3
Sebastian <sebiwrn@web.de>
2025-10-16 08:58:35 +0200

    Adapted bitrates

--- pkg/runner_manager/manager.go
+++ pkg/runner_manager/manager.go
@@ -253,7 +253,7 @@ func (m *Manager) requestStreamVersion(ctx context.Context, s model.Stream, clie
 		return nil, fmt.Errorf("invalid stream version %v", version)
 	}
 
-	var outputOptions = "-c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -b:a 128k -g 60 -b:v 4000k"
+	var outputOptions = "-c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -b:a 128k -g 60 -bufsize 5000k -maxrate 5000k -b:v 3500k"
 	if !s.IsSelfStream() {
 		switch lh.StreamProtocol {
 		case model.RTSP:
