843281a96da9db3ae0d371933fbe56e5f0c18032
carlobortolan <carlobortolan@gmail.com>
2025-10-17 16:28:37 +0900

    Refactor ffmpeg command

--- worker/worker/stream.go
+++ worker/worker/stream.go
@@ -24,30 +24,16 @@ func stream(streamCtx *StreamContext) {
 	lastErr := time.Now().Add(time.Minute * -1)
 	errCount := 0
 	for time.Now().Before(streamUntil) && !streamCtx.stopped {
-		var cmd *exec.Cmd
-
-		// todo: too much duplication
-		if strings.Contains(streamCtx.sourceUrl, "rtsp") {
-			timeout := fmt.Sprintf("%.0f", time.Until(streamUntil).Seconds())
-			cmdStr := fmt.Sprintf(
-				`ffmpeg -hide_banner -nostats -rtsp_transport tcp -t %s -i %s -map 0 -c copy -f mpegts - -c:v libx264 -preset veryfast -tune zerolatency -maxrate 2500k -bufsize 3000k -g 60 -r 30 -x264-params keyint=60:scenecut=0 -c:a aac -ar 44100 -b:a 128k -f flv %s >> %s`,
-				timeout,
-				streamCtx.sourceUrl,
-				fmt.Sprintf("%s/%s", streamCtx.ingestServer, streamCtx.streamName),
-				streamCtx.getRecordingFileName(),
-			)
-			cmd = exec.Command("sh", "-c", cmdStr)
+		timeout := fmt.Sprintf("%.0f", time.Until(streamUntil).Seconds())
+		args := buildFFmpegArgs(streamCtx, timeout)
+		cmd := exec.Command("ffmpeg", args...)
+		recordFile, err := os.OpenFile(streamCtx.getRecordingFileName(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err == nil {
+			cmd.Stdout = recordFile
 		} else {
-			timeout := fmt.Sprintf("%.0f", time.Until(streamUntil).Seconds())
-			cmdStr := fmt.Sprintf(
-				`ffmpeg -hide_banner -nostats -rw_timeout 5000000 -t %s -i %s -map 0 -c copy -f mpegts - -c:v libx264 -preset veryfast -tune zerolatency -maxrate 2500k -bufsize 3000k -g 60 -r 30 -x264-params keyint=60:scenecut=0 -c:a aac -ar 44100 -b:a 128k -f flv %s >> %s`,
-				timeout,
-				streamCtx.sourceUrl,
-				fmt.Sprintf("%s/%s", streamCtx.ingestServer, streamCtx.streamName),
-				streamCtx.getRecordingFileName(),
-			)
-			cmd = exec.Command("sh", "-c", cmdStr)
+			log.WithError(err).Error("Could not open file for ffmpeg stdout")
 		}
+
 		// persist stream command in context, so it can be killed later
 		streamCtx.streamCmd = cmd
 		log.WithField("cmd", cmd.String()).Info("Starting stream")
@@ -59,7 +45,15 @@ func stream(streamCtx *StreamContext) {
 		}
 		// Create a new pgid for the new process, so we don't kill the parent process when ending the stream
 		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-		err := cmd.Run()
+
+		err = cmd<.Run()
+		if recordFile != nil {
+			_ = recordFile.Close()
+		}
+		if ffmpegErr != nil {
+			_ = ffmpegErr.Close()
+		}
+
 		if err != nil && !streamCtx.stopped {
 			errCount++
 			if errCount > 20 && strings.Contains(streamCtx.sourceUrl, "localhost") {
@@ -78,6 +72,38 @@ func stream(streamCtx *StreamContext) {
 		}
 	}
 	streamCtx.streamCmd = nil
+}
+
+func buildFFmpegArgs(streamCtx *StreamContext, timeout string) []string {
+	args := []string{
+		"-hide_banner", "-nostats",
+		"-t", timeout,
+		"-i", streamCtx.sourceUrl,
+		"-map", "0",
+		"-c", "copy",
+		"-f", "mpegts", "-",
+		"-c:v", "libx264",
+		"-preset", "veryfast",
+		"-tune", "zerolatency",
+		"-maxrate", "2500k",
+		"-bufsize", "3000k",
+		"-g", "60",
+		"-r", "30",
+		"-x264-params", "keyint=60:scenecut=0",
+		"-c:a", "aac",
+		"-ar", "44100",
+		"-b:a", "128k",
+		"-f", "flv",
+		fmt.Sprintf("%s/%s", streamCtx.ingestServer, streamCtx.streamName),
+	}
+
+	if strings.Contains(streamCtx.sourceUrl, "rtsp") {
+		args = append([]string{"-rtsp_transport", "tcp"}, args...)
+	} else {
+		args = append(args, "-rw_timeout", "5000000")
+	}
+
+	return args
 }
 
 // errorWithBackoff updates lastError and sleeps for a second if the last error was within this second
