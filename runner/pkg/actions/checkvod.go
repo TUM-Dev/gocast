409289cdfba2733d059ebe10eae3db97a763a4da
Joscha Henningsen <joschahenn@gmail.com>
2025-10-29 19:09:39 +0100

    chore(runner/checkvod): minor code cleanup

--- runner/pkg/actions/checkvod.go
+++ runner/pkg/actions/checkvod.go
@@ -17,27 +17,20 @@ import (
 
 // CheckVoD probes both the livestream and the VoD and checks if they are identical.
 func CheckVoD(ctx context.Context, logger *slog.Logger, _ chan *protobuf.Notification, d map[string]any, metrics *metrics.Broker) error {
-	err, duration := checkVoD(ctx, logger, d, metrics)
+	duration, err := checkVoD(ctx, logger, d, metrics)
 	recordingDir, ok := d["recordingDir"].(string)
 	if !ok {
-		return AbortingError(err)
+		return AbortingError(fmt.Errorf("missing recordingDir"))
 	}
-	formatString := ".del-%s"
+	formatString := ".del-%d"
 	if err != nil {
 		// Add a keep flag to the folder, add duration of 2h on average to the keep
-		formatString = ".keep-%s"
-		newDuration, err3 := time.ParseDuration("2h")
-		if err3 != nil {
-			err = errors.Join(err, err3)
-		}
-		duration = newDuration
+		formatString = ".keep-%d"
+		duration = 2 * time.Hour
 	}
-	filename := fmt.Sprintf(formatString, time.Now().Add(duration).Format(time.RFC3339))
+	filename := fmt.Sprintf(formatString, time.Now().Add(duration).Unix())
 	err2 := writeManagementFile(recordingDir, filename)
-	if err2 != nil {
-		errors.Join(err, err2)
-	}
-	return err
+	return errors.Join(err, err2)
 }
 
 func writeManagementFile(recordingDir string, filename string) error {
@@ -52,25 +45,25 @@ func writeManagementFile(recordingDir string, filename string) error {
 	return nil
 }
 
-func checkVoD(ctx context.Context, logger *slog.Logger, d map[string]any, metrics *metrics.Broker) (error, time.Duration) {
+func checkVoD(ctx context.Context, logger *slog.Logger, d map[string]any, metrics *metrics.Broker) (time.Duration, error) {
 	const DurationToleranceSeconds = 10.0
 
 	streamID, ok := d["streamID"].(uint64)
 	if !ok {
-		return AbortingError(fmt.Errorf("no stream id in context")), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("no stream id in context"))
 	}
 	streamVersion, ok := d["streamVersion"].(string)
 	if !ok {
-		return AbortingError(fmt.Errorf("no stream version in context")), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("no stream version in context"))
 	}
 	recordingDir, ok := d["recordingDir"].(string)
 	if !ok {
-		return AbortingError(fmt.Errorf("no recordingDir in context")), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("no recordingDir in context"))
 	}
 	recording := path.Join(recordingDir, "playlist.m3u8")
 	vod, ok := d["vodPath"].(string)
 	if !ok {
-		return AbortingError(fmt.Errorf("no vodPath in context")), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("no vodPath in context"))
 	}
 
 	isHealthy := false
@@ -83,24 +76,24 @@ func checkVoD(ctx context.Context, logger *slog.Logger, d map[string]any, metric
 
 	probe, err := ffmpeg.Probe(ctx, recording)
 	if err != nil {
-		return AbortingError(fmt.Errorf("probe livestream: %w", err)), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("probe livestream: %w", err))
 	}
 	liveDuration := probe.Duration()
 	probe, err = ffmpeg.Probe(ctx, vod)
 	if err != nil {
-		return AbortingError(fmt.Errorf("probe vod: %w", err)), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("probe vod: %w", err))
 	}
 	vodDuration := probe.Duration()
 	if math.Abs(liveDuration-vodDuration) > DurationToleranceSeconds {
-		return AbortingError(fmt.Errorf("livestream duration (%f) and recording duration (%f) diverge by more than 10 s", liveDuration, vodDuration)), time.Duration(0)
+		return time.Duration(0), AbortingError(fmt.Errorf("livestream duration (%f) and recording duration (%f) diverge by more than 10 s", liveDuration, vodDuration))
 	}
 	isHealthy = true
 	logger.With("stream", streamID, "stream_version", streamVersion).Info("CheckVoD successful")
 
 	duration, err := time.ParseDuration(fmt.Sprintf("%.0fs", liveDuration))
 	if err != nil {
-		return AbortingError(err), time.Duration(0)
+		return time.Duration(0), AbortingError(err)
 	}
 
-	return nil, duration
+	return duration, nil
 }
