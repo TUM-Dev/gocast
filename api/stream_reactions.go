c457cc8174fa4cff236193e2c66b0e3d9c026779
Maziar <107628584+Mazi02@users.noreply.github.com>
2026-06-27 13:05:10 +0200

    Update api/stream_reactions.go
    Co-authored-by: coolgirl19 <35705443+coolgirl19@users.noreply.github.com>

--- api/stream_reactions.go
+++ api/stream_reactions.go
@@ -377,7 +377,7 @@ func NotifyAdminsOnReactionPercentages(context context.Context) {
 	streamReactionPercentages := map[uint]map[string]float64{}
 
 	for stream := range targetsByStream {
-		reactionsRaw, err := daoWrapper.StreamReactionDao.GetByStreamWithinMinutes(context, stream, 2) // TODO: Make this variable for the lecturer
+		reactionsRaw, err := daoWrapper.StreamReactionDao.GetByStreamWithinMinutes(context, stream, 15) // TODO: Make this variable for the lecturer
 		if err != nil {
 			logger.Error("could not get reactions for stream", "stream", stream, "err", err)
 			return
