628cde2b92c3cbd5e5c248bd94481d9a5b81f928
Mazi02 <shaterimaziar2@gmail.com>
2026-06-30 15:42:55 +0200

    bash-services-local v1 for linux

--- bash-services-local.sh
+++ bash-services-local.sh
@@ -11,8 +11,23 @@ cleanup() {
     pkill -f "worker/edge" || true
     pkill -f "vod-service/cmd/vod-service" || true
     pkill -f "mediamtx" || true
+    kill -9 $(pgrep -o main)
+    echo "Runner ended."
 
     docker stop meilisearch mariadb-tumlive meilisearch >/dev/null 2>&1 || true
+
+    # restore dev changes
+    sed -i 's/ListenAndServe(":8080"/ListenAndServe(":8089"/' vod-service/internal/vodService.go
+
+    sed -i "s|var vodPath = .*|var vodPath = \"/vod\"|" worker/edge/edge.go
+
+    sed -i 's/^token: abc//' config.yaml
+
+    sed -i 's|^#externalAuthenticationURL: https://tum.live/api/selfstream/onPublish|externalAuthenticationURL: https://tum.live/api/selfstream/onPublish|' ingest/mediamtx.yml
+
+    sed -i 's|^externalAuthenticationURL: http://localhost:8081/api/selfstream/onPublish|#externalAuthenticationURL: http://localhost:8081/api/selfstream/onPublish|' ingest/mediamtx.yml
+
+    sed -Ei 's|^ingestbase:.*|ingestbase: rtmp://ingest.tum.live/|' config.yaml
 
     echo "Done."
     exit 0
@@ -20,17 +35,21 @@ cleanup() {
 
 trap cleanup INT TERM
 
-MASS=/home/mkm00/dev/storage/mass
-LIVE=/home/mkm00/dev/storage/live
+MASS="$HOME/dev/storage/mass"
+LIVE="$HOME/dev/storage/live"
 
 sed -i 's/ListenAndServe(":8089"/ListenAndServe(":8080"/' vod-service/internal/vodService.go
 sed -i "s|var vodPath = .*|var vodPath = \"$MASS\"|" worker/edge/edge.go
 
 grep -q '^token:' config.yaml && sed -i 's/^token:.*/token: abc/' config.yaml || echo 'token: abc' >> config.yaml
-sed -Ei 's|^([[:space:]]*)externalAuthenticationURL:|\1#externalAuthenticationURL:|' ingest/mediamtx.yml
-sed -Ei 's|^([[:space:]]*)#externalAuthenticationURL:[[:space:]]*http://localhost:8081/api/selfstream/onPublish|\1externalAuthenticationURL: http://localhost:8081/api/selfstream/onPublish|' ingest/mediamtx.yml
-sed -Ei 's|http://localhost:8081|http://127.0.0.1:8081|' ingest/mediamtx.yml
-sed -Ei 's|^ingestbase:.*|ingestbase: rtmp://127.0.0.1|' config.yaml
+
+# Comment production
+sed -i 's|^externalAuthenticationURL: https://tum.live/api/selfstream/onPublish|#externalAuthenticationURL: https://tum.live/api/selfstream/onPublish|' ingest/mediamtx.yml
+
+# Uncomment local
+sed -i 's|^#externalAuthenticationURL: http://localhost:8081/api/selfstream/onPublish|externalAuthenticationURL: http://localhost:8081/api/selfstream/onPublish|' ingest/mediamtx.yml
+
+sed -Ei 's|^ingestbase:.*|ingestbase: rtmp://localhost|' config.yaml
 
 docker start meilisearch mariadb-tumlive
 
@@ -66,6 +85,10 @@ mediamtx ./ingest/mediamtx.yml &
 sleep 2
 echo "mediamtx ready"
 
+OUTPUT_DIR="$MASS" go run ./vod-service/cmd/vod-service &
+until nc -z localhost 8080; do sleep 1; done
+echo "vod-service ready"
+
 LrzUploadUrl="http://localhost:8080" \
 VodURLTemplate="http://localhost:8089/vod/%s.mp4/playlist.m3u8" \
 Token=abc \
@@ -74,9 +97,6 @@ go run ./worker/cmd/worker &
 sleep 2
 echo "worker started"
 
-go run ./vod-service/cmd/vod-service &
-until nc -z localhost 8080; do sleep 1; done
-echo "vod-service ready"
 
 go run ./worker/edge &
 until nc -z localhost 8089; do sleep 1; done
