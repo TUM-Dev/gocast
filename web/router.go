c70f2bfbc7bcf4e6194db6ab3fa84423860b0cd3
Joscha Henningsen <joschahenn@gmail.com>
2026-08-30 15:20:52 +0200

    refactor: Delete the server-rendered runners page

--- web/router.go
+++ web/router.go
@@ -313,7 +313,7 @@ func configMainRoute(router *gin.Engine) {
 	serverAdminGroup.GET("/admin/lecture-halls", routes.AdminPage)
 	serverAdminGroup.GET("/admin/lecture-halls/new", routes.AdminPage)
 	serverAdminGroup.GET("/admin/workers", routes.AdminPage)
-	registerPage(serverAdminGroup, http.MethodGet, "/admin/runners", routes.AdminPage)
+	registerPage(serverAdminGroup, http.MethodGet, "/admin/runners", nil)
 	serverAdminGroup.GET("/admin/server-notifications", routes.AdminPage)
 	serverAdminGroup.GET("/admin/server-stats", routes.AdminPage)
 	serverAdminGroup.GET("/admin/course-import", routes.AdminPage)
