0bb54a773f0913e55fb6335c3bb1d9379f33ca0f
Mazi02 <shaterimaziar2@gmail.com>
2026-09-12 11:03:54 +0200

    removing pointer passing in AddDocumentsInBatches

--- tools/meiliExporter.go
+++ tools/meiliExporter.go
@@ -152,7 +152,7 @@ func (m *MeiliExporter) Export() {
 				Visibility:   course.Visibility,
 			}
 		}
-		_, err := coursesIndex.AddDocumentsInBatches(&meilicourses, 500, docOptions("ID"))
+		_, err := coursesIndex.AddDocumentsInBatches(meilicourses, 500, docOptions("ID"))
 		if err != nil {
 			logger.Error("issue adding courses to meili", "err", err)
 		}
